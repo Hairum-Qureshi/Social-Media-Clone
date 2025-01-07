bf2bd75c52e778befaf3c390142f7ccf6d088018
Hairum Qureshi <hqureshi@udel.edu>
2025-01-07 12:52:54 -0500

    created UserData interface

--- api/interfaces.ts
+++ api/interfaces.ts
@@ -1,17 +1,29 @@
 import { Types } from "mongoose";
 
 export interface IUser {
-    _id: Types.ObjectId,
-    username: string,
-    fullName: string,
-    password: string,
-    email: string,
-    followers: Types.ObjectId[],
-    following: Types.ObjectId[],
-    profilePicture: string,
-    coverImage: string,
-    bio: string,
-    link: string,
-    createdAt: Date,
-    updatedAt: Date
+	_id: Types.ObjectId;
+	username: string;
+	fullName: string;
+	password: string;
+	email: string;
+	followers: Types.ObjectId[];
+	following: Types.ObjectId[];
+	profilePicture: string;
+	coverImage: string;
+	bio: string;
+	link: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface UserData {
+	_id: Types.ObjectId;
+	username: string;
+	fullName: string;
+	email: string;
+	followers: Types.ObjectId[];
+	following: Types.ObjectId[];
+	profilePicture: string;
+	coverImage: string;
+    bio: string;
 }
