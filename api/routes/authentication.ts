02e00c871cf4741cc13fac829d9611f335ba216f
Hairum Qureshi <hqureshi@udel.edu>
2025-01-07 13:11:07 -0500

    changed /sign-out' route to be a POST request instead of a GET request and created a new GET request for getting the current user

--- api/routes/authentication.ts
+++ api/routes/authentication.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { signIn, signOut, signUp } from "../controllers/authentication";
+import { signUp, signIn, signOut, getCurrentUser } from "../controllers/authentication";
+import checkAuthStatus from "../middleware/checkAuthStatus";
 
 const router = express.Router();
 
-router.post('/sign-up', signUp);
+router.post('/sign-up', checkAuthStatus, signUp);
 router.post('/sign-in', signIn);
-router.get('/sign-out', signOut);
+router.post('/sign-out', signOut);
+router.get('/current-user', getCurrentUser);
 
 export default router;
