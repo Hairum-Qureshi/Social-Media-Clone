581cd9c93b623524f3557cbdd88cf31fc891ed2a
Hairum Qureshi <hqureshi@udel.edu>
2025-01-08 08:27:01 -0500

    removed followand unfollow user routes and controller functions and combined it into one: handleFollowStatus constructor and 'follow-status' route

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import { followUser, getProfile, getSuggestedUsers, unfollowUser, updateProfile } from "../controllers/user";
+import { getProfile, getSuggestedUsers, handleFollowStatus, updateProfile } from "../controllers/user";
 import checkAuthStatus from "../middleware/checkAuthStatus";
 
 const router = express.Router();
 
 router.get('/profile/:username', checkAuthStatus, getProfile);
 router.get('/suggested', checkAuthStatus, getSuggestedUsers);
-router.post('/follow/:uid', checkAuthStatus, followUser);
-router.post('/unfollow/:uid', checkAuthStatus, unfollowUser);
+router.post('/follow-status/:uid', checkAuthStatus, handleFollowStatus);
 router.post('/update-profile', checkAuthStatus, updateProfile);
 
 export default router;
