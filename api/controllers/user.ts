ed9c83b55c8c27d8695a9f3647956faf76252966
Hairum Qureshi <hqureshi@udel.edu>
2025-01-07 22:28:52 -0500

    created all the starter user route controller functions

---
+++ api/controllers/user.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+const getProfile = async (req:Request, res:Response) => {
+
+}
+
+const getSuggestedUsers = async (req:Request, res:Response) => {
+
+}
+
+const followUser = async (req:Request, res:Response) => {
+
+}
+
+const unfollowUser = async (req:Request, res:Response) => {
+
+}
+
+const updateProfile = async (req:Request, res:Response) => {
+
+}
+
+export { getProfile, getSuggestedUsers, followUser, unfollowUser, updateProfile };
