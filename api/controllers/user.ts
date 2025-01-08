e5f5a3892eec2508396215cc36aa6719af697db6
Hairum Qureshi <hqureshi@udel.edu>
2025-01-08 08:59:55 -0500

    implemented follow/unfollow functionality inside of 'handleFollowStatus' controller function

--- api/controllers/user.ts
+++ api/controllers/user.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { IUser } from "../interfaces";
 import User from "../models/User";
+import mongoose from "mongoose";
 
 const getProfile = async (req: Request, res: Response): Promise<void> => {
 	try {
-        const { username } = req.params;
+		const { username } = req.params;
 		const user: IUser = (await User.findOne({ username }).select(
 			"-password -__v"
 		)) as IUser;
@@ -24,29 +25,109 @@ const getProfile = async (req: Request, res: Response): Promise<void> => {
 	}
 };
 
-const getSuggestedUsers = async (req: Request, res: Response) => {
+const getSuggestedUsers = async (req: Request, res: Response) => {};
 
-};
+const handleFollowStatus = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const { uid } = req.params;
+		const userToModify: IUser = (await User.findById({
+			_id: uid
+		})) as IUser;
+		const currUID:string = req.user._id.toString();
+		const currentUser: IUser = (await User.findById({
+			_id: currUID
+		})) as IUser;
 
-const handleFollowStatus = async (req: Request, res: Response) => {
-    try {
-        const { uid } = req.params;
 
+		if (uid === currUID) {
+			res.status(400).json({ error: "You cannot follow/unfollow yourself" });
+			return;
+		}
 
-    } catch (error) {
-        console.error(
+		if (!userToModify || !currentUser) {
+			res.status(404).json({ message: "User not found" });
+			return;
+		}
+
+		const uidToObjectID = new mongoose.Types.ObjectId(uid);
+
+		if (!mongoose.isValidObjectId(uidToObjectID)) {
+			res.status(400).json({ error: "Invalid user ID" });
+			return;
+		}
+
+		const isFollowing = currentUser.following.includes(uidToObjectID);
+
+		if (isFollowing) {
+			// unfollow the user
+			await User.findByIdAndUpdate(
+				{
+					_id: uid
+				},
+				{
+					$pull: {
+						followers: currUID
+					}
+				}
+			);
+
+			await User.findByIdAndUpdate(
+				{
+					_id: currUID
+				},
+				{
+					$pull: {
+						following: uid
+					}
+				}
+			);
+
+			res.status(200).json({
+				message: "User unfollowed successfully"
+			});
+			return;
+		} else {
+			// follow the user
+
+			await User.findByIdAndUpdate(
+				{
+					_id: uid
+				},
+				{
+					$push: {
+						followers: currUID
+					}
+				}
+			);
+
+			await User.findByIdAndUpdate(
+				{
+					_id: currUID
+				},
+				{
+					$push: {
+						following: uid
+					}
+				}
+			);
+		}
+
+		res.status(200).json({
+			message: "User followed successfully"
+		});
+		return;
+	} catch (error) {
+		console.error(
 			"Error in user.ts file, followUser function controller".red.bold,
 			error
 		);
 		res.status(500).json({ message: (error as Error).message });
-    }
+	}
 };
 
 const updateProfile = async (req: Request, res: Response) => {};
 
-export {
-	getProfile,
-	getSuggestedUsers,
-	handleFollowStatus,
-	updateProfile
-};
+export { getProfile, getSuggestedUsers, handleFollowStatus, updateProfile };
