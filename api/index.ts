776ebee5b97a8a4194da4027fa167bdc748e3619
Hairum Qureshi <hqureshi@udel.edu>
2025-01-10 18:58:28 -0500

    added cloudinary API config

--- api/index.ts
+++ api/index.ts
@@ -6,9 +6,16 @@ import colors from "colors";
 import authentication from "./routes/authentication";
 import mongoose from "mongoose";
 import user from "./routes/user";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 colors.enable();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 
@@ -18,15 +25,15 @@ const corsOptions = {
 	optionSuccessStatus: 200
 };
 
-app.use(cors(corsOptions)); 
-app.use(cookieParser()); 
-app.use(express.json()); 
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authentication);
 app.use("/api/user", user);
 
-const PORT: number = +process.env.PORT! || 3000;  
+const PORT: number = +process.env.PORT! || 3000;
 
 app.listen(PORT, () => {
 	const connectToMongoDB = async () => {
