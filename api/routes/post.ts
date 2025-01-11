0b1b3e0c51a6e6dccf12024df38f85d2b56ee27f
Hairum Qureshi <hqureshi@udel.edu>
2025-01-11 16:58:33 -0500

    modified like route so it handles both liking and unliking a post

--- api/routes/post.ts
+++ api/routes/post.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { createPost, deletePost, likePost, postComment } from "../controllers/post";
+import { createPost, deletePost, handleLikes, postComment } from "../controllers/post";
 
 const router = express.Router();
 
 router.post("/create", checkAuthStatus, createPost);
 router.delete("/:postID", checkAuthStatus, deletePost);
-router.post("/like/:postID", checkAuthStatus, likePost);
+router.post("/handle-likes/:postID", checkAuthStatus, handleLikes);
 router.post("/comment/:postID", checkAuthStatus, postComment);
+// router.delete("/comment/:postID", checkAuthStatus, deleteComment);
 
 export default router;
