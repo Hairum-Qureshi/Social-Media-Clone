391f0933d4411ade9480876b00688a3193c6e748
Hairum Qureshi <hqureshi@udel.edu>
2025-01-10 21:01:13 -0500

    created post controller function skeletons and exported them

---
+++ api/controllers/post.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+const createPost = async (req: Request, res: Response) => {};
+
+const deletePost = async (req: Request, res: Response) => {};
+
+const likePost = async (req: Request, res: Response) => {};
+
+const postComment = async (req: Request, res: Response) => {};
+
+export { createPost, deletePost, likePost, postComment };
