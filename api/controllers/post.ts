22d0c0b35104ee9e2b7d0e0648c68298652d07ff
Hairum Qureshi <hqureshi@udel.edu>
2025-01-11 16:24:00 -0500

    created deletePost controller function

--- api/controllers/post.ts
+++ api/controllers/post.ts
@@ -17,15 +17,15 @@ const createPost = async (req: Request, res: Response): Promise<void> => {
 		}
 
 		if (image) {
-            const uploadedImage = await cloudinary.uploader.upload(image);
-            image = uploadedImage.secure_url;
+			const uploadedImage = await cloudinary.uploader.upload(image);
+			image = uploadedImage.secure_url;
 		}
 
-		const newPost:IPost = await Post.create({
+		const newPost: IPost = await Post.create({
 			user: currUID,
 			text,
 			image
-		})
+		});
 
 		res.status(201).json(newPost);
 	} catch (error) {
@@ -37,7 +37,46 @@ const createPost = async (req: Request, res: Response): Promise<void> => {
 	}
 };
 
-const deletePost = async (req: Request, res: Response) => {};
+const deletePost = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const postID: string = req.params.postID;
+		const post = await Post.findById({ _id: postID });
+		if (!post) {
+			res.status(404).json({ message: "Post not found" });
+			return;
+		}
+
+		const currUID: string = req.user._id.toString();
+		if (post.user.toString() !== currUID) {
+			res
+				.status(403)
+				.json({ message: "You're not authorized to delete this post" });
+			return;
+		}
+
+		if (post.image) {
+			const imageID: string | undefined = post.image
+				.split("/")
+				.pop()
+				?.split(".")[0];
+			if (imageID) {
+				await cloudinary.uploader.destroy(imageID);
+			}
+		}
+
+		await Post.findByIdAndDelete({
+			_id: postID
+		});
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (error) {
+		console.error(
+			"Error in post.ts file, deletePost function controller".red.bold,
+			error
+		);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
 
 const likePost = async (req: Request, res: Response) => {};
 
