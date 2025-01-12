eaf428f726e1e25d720faefd125ec07cd0914c05
Hairum Qureshi <hqureshi@udel.edu>
2025-01-12 10:43:08 -0500

    registered custom font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				Archivo: ["Archivo Black", "serif"]
+			}
+		}
 	},
 	plugins: []
 };
