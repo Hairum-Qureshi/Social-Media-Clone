c156d73b5ca90f9d9c34eb0830fc958998041a42
Hairum Qureshi <hqureshi@udel.edu>
2025-01-11 23:48:23 -0500

    created 'all posts' route

--- api/routes/post.ts
+++ api/routes/post.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { createPost, deletePost, handleLikes, postComment } from "../controllers/post";
+import { createPost, deletePost, handleLikes, postComment, getAllPosts } from "../controllers/post";
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.post("/create", checkAuthStatus, createPost);
 router.delete("/:postID", checkAuthStatus, deletePost);
 router.post("/handle-likes/:postID", checkAuthStatus, handleLikes);
 router.post("/comment/:postID", checkAuthStatus, postComment);
+router.get("/all", checkAuthStatus, getAllPosts);
 // router.delete("/comment/:postID", checkAuthStatus, deleteComment);
 
 export default router;
