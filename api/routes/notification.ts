b2655048edbc2cfbd18a9b623b0de68c42ae2361
Hairum Qureshi <hqureshi@udel.edu>
2025-01-12 09:04:44 -0500

    added route to delete notification by ID

--- api/routes/notification.ts
+++ api/routes/notification.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { deleteNotifications, getAllNotifications } from "../controllers/notification";
+import { getAllNotifications, deleteNotification, deleteNotifications } from "../controllers/notification";
 
 const router = express.Router();
 
 router.get('/all', checkAuthStatus, getAllNotifications);
+router.delete("/:notificationID", checkAuthStatus, deleteNotification);
 router.delete('/all', checkAuthStatus, deleteNotifications);
 
 export default router;
