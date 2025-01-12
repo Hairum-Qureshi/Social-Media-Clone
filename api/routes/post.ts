b5e969820362c6676ab2d27a11f8cb5d5c6a0214
Hairum Qureshi <hqureshi@udel.edu>
2025-01-12 00:53:30 -0500

    created route to get all liked posts

--- api/routes/post.ts
+++ api/routes/post.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { createPost, deletePost, handleLikes, postComment, getAllPosts } from "../controllers/post";
+import { createPost, deletePost, handleLikes, postComment, getAllPosts, getAllLikedPosts } from "../controllers/post";
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.delete("/:postID", checkAuthStatus, deletePost);
 router.post("/handle-likes/:postID", checkAuthStatus, handleLikes);
 router.post("/comment/:postID", checkAuthStatus, postComment);
 router.get("/all", checkAuthStatus, getAllPosts);
+router.get("/liked-posts", checkAuthStatus, getAllLikedPosts);
 // router.delete("/comment/:postID", checkAuthStatus, deleteComment);
 
 export default router;
