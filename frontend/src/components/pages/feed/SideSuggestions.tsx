0aa44b574255aa2dc06ccd4b63946a598cbde993
Hairum Qureshi <hqureshi@udel.edu>
2025-01-14 18:01:02 -0500

    added div to display the list of suggested users the current user should follow

--- frontend/src/components/pages/feed/SideSuggestions.tsx
+++ frontend/src/components/pages/feed/SideSuggestions.tsx
@@ -1,5 +1,16 @@
+import UserCard from "./UserCard";
+
 export default function SideSuggestions() {
 	return (
-		<div className="lg:w-1/4 w-32 bg-black border-l-2 border-l-gray-700 h-screen"></div>
+		<div className="lg:w-1/4 w-32 bg-black border-l-2 border-l-gray-700 overflow-hidden text-white">
+			<div className="rounded-md bg-gray-800 my-5 mx-3 p-2">
+				<h1 className="font-bold">Who to follow</h1>
+				<UserCard />
+				<UserCard />
+				<UserCard />
+				<UserCard />
+				<UserCard />
+			</div>
+		</div>
 	);
 }
