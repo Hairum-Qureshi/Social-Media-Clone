03f81c903489bf04fd657f68259f1033f6a52ca7
Hairum Qureshi <hqureshi@udel.edu>
2025-01-15 11:39:42 -0500

    added conditional rendering for displaying the close button if 'allowDelete' is true

--- frontend/src/components/pages/feed/editor-tools/carousel/CarouselImage.tsx
+++ frontend/src/components/pages/feed/editor-tools/carousel/CarouselImage.tsx
@@ -2,15 +2,23 @@ import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CarouselImageProps } from "../../../../../interfaces";
 
-export default function CarouselImage({image, imageIndex, removeImage}:CarouselImageProps) {
+export default function CarouselImage({
+	image,
+	imageIndex,
+	removeImage,
+	allowDelete
+}: CarouselImageProps) {
 	return (
 		<div className="w-full h-full m-2 relative">
-			<div className="absolute top-0 right-0 text-lg text-white p-2 bg-black rounded-full flex items-center justify-center h-8 w-8 m-2 hover:cursor-pointer" onClick = {() => removeImage(imageIndex)}>
+			{allowDelete && <div
+				className="absolute top-0 right-0 text-lg text-white p-2 bg-black rounded-full flex items-center justify-center h-8 w-8 m-2 hover:cursor-pointer"
+				onClick={() => removeImage(imageIndex)}
+			>
 				<FontAwesomeIcon icon={faX} />
-			</div>
+			</div>}
 			<img
 				src={image}
-				className="object-cover w-full h-full rounded-md"
+				className="object-contain rounded-md"
 				alt="Uploaded image"
 			/>
 		</div>
