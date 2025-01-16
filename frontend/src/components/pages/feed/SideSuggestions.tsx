26b4348ae1ce5a42d0a42ceef25d8cbb842e4590
Hairum Qureshi <hqureshi@udel.edu>
2025-01-15 20:59:32 -0500

    moved suggested followers div into a separate component

--- frontend/src/components/pages/feed/SideSuggestions.tsx
+++ frontend/src/components/pages/feed/SideSuggestions.tsx
@@ -1,16 +1,9 @@
-import UserCard from "./UserCard";
+import FollowersSuggestions from "./editor-tools/FollowersSuggestions";
 
 export default function SideSuggestions() {
 	return (
 		<div className="lg:w-1/4 w-32 bg-black border-l-2 border-l-gray-700 overflow-hidden text-white">
-			<div className="rounded-md bg-gray-800 my-5 mx-3 p-2 hidden lg:block">
-				<h1 className="font-bold">Who to follow</h1>
-				<UserCard />
-				<UserCard />
-				<UserCard />
-				<UserCard />
-				<UserCard />
-			</div>
+			<FollowersSuggestions />
 		</div>
 	);
 }
