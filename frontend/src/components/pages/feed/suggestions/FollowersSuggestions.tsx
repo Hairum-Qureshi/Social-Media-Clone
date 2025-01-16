f57d3ad805f80ecca15b867c3449c7d5e7184fd7
Hairum Qureshi <hqureshi@udel.edu>
2025-01-16 00:57:54 -0500

    added some more margin and increased the title font

--- frontend/src/components/pages/feed/suggestions/FollowersSuggestions.tsx
+++ frontend/src/components/pages/feed/suggestions/FollowersSuggestions.tsx
@@ -3,7 +3,7 @@ import UserCard from "../UserCard";
 export default function FollowersSuggestions() {
 	return (
 		<div className="rounded-md bg-zinc-900 my-5 mx-3 p-2 hidden lg:block">
-			<h1 className="font-bold">Who to follow</h1>
+			<h1 className="font-bold text-xl m-3">Who to follow</h1>
 			<UserCard />
 			<UserCard />
 			<UserCard />
