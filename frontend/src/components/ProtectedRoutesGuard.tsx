a94b68e01e5e8a26e7cd688f54de16b3a1fe76f7
Hairum Qureshi <hqureshi@udel.edu>
2025-03-03 13:42:33 -0500

    implemented logic for checking whether or not userData is null; if it's null, it'll redirect the user back to the sign-in page, otherwise, it will render the feed component

--- frontend/src/components/ProtectedRoutesGuard.tsx
+++ frontend/src/components/ProtectedRoutesGuard.tsx
@@ -1,17 +1,27 @@
 import { Navigate } from "react-router-dom";
 import SideNavbar from "./pages/navigation/SideNavbar";
-import SideSuggestions from "./pages/feed/SideSuggestions";
+import SideSuggestions from "./pages/feed/suggestions/SideSuggestions";
+import useAuthContext from "../contexts/AuthContext";
+import { useEffect, useState } from "react";
 
 export default function ProtectedRoutesGuard({
 	children
 }: React.PropsWithChildren) {
-	const isLoggedIn = true;
-	return isLoggedIn ? (
+	const { userData } = useAuthContext()!;
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		if (userData !== null) {
+			setIsLoading(false);
+		}
+	}, [userData]);
+
+	if (isLoading) return null;
+
+	return userData ? (
 		<div className="w-full h-screen flex">
 			<SideNavbar />
-			<div className="flex-1 flex justify-center">
-				{children}
-			</div>
+			<div className="flex-1 flex justify-center">{children}</div>
 			<SideSuggestions />
 		</div>
 	) : (
