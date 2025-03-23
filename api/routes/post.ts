b548b9f987fa71997814b3ed65a1e6878d1c1561
Hairum Qureshi <hqureshi@udel.edu>
2025-03-23 09:24:50 -0400

    created new GET request route to get all the current user's posts

--- api/routes/post.ts
+++ api/routes/post.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { createPost, deletePost, handleLikes, postComment, getAllPosts, getAllLikedPosts, getFollowingUsersPosts, getUserPosts, deleteComment } from "../controllers/post";
+import { createPost, deletePost, handleLikes, postComment, getAllPosts, getAllLikedPosts, getFollowingUsersPosts, getUserPosts, deleteComment, getAllYourPosts } from "../controllers/post";
 
 const router = express.Router();
 
@@ -12,6 +12,7 @@ router.get("/all", checkAuthStatus, getAllPosts);
 router.get("/liked-posts/:userID", checkAuthStatus, getAllLikedPosts);
 router.get("/following", checkAuthStatus, getFollowingUsersPosts);
 router.get("/user/:username", checkAuthStatus, getUserPosts);
+router.get("/current-user/all", checkAuthStatus, getAllYourPosts);
 router.delete("/comment/:commentID/:postID", checkAuthStatus, deleteComment);
 
 export default router;
