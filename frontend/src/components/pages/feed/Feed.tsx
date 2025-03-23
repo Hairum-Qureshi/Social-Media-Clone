495291c0369107492ce92b8fa1b27a65903454bf
Hairum Qureshi <hqureshi@udel.edu>
2025-03-23 08:59:26 -0400

    wrapped Post component in Link tag to redirect user to that post once clicked and updated Post's props to be just 'isOwner' and postData

--- frontend/src/components/pages/feed/Feed.tsx
+++ frontend/src/components/pages/feed/Feed.tsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import Editor from "./editor-tools/Editor";
+import Post from "./editor-tools/Post";
+import useAuthContext from "../../../contexts/AuthContext";
+import usePosts from "../../../hooks/usePosts";
+import { Post as IPost } from "../../../interfaces";
+import { Link } from "react-router-dom";
 
 export default function Landing() {
 	const [isForYou, setIsForYou] = useState(true);
 	const [isFollowing, setIsFollowing] = useState(false);
+	const { userData } = useAuthContext()!;
 
+	const { postData, loadingStatus } = usePosts(
+		isForYou ? "For You" : isFollowing ? "Following" : ""
+	);
+
+	// TODO - for the navigation bar, make sure to add a blue dot to symbolize notifications
 	// TODO - add feature where if you're in that route, the icon should be filled in, otherwise it shouldn't
 	// TODO - make sure it's mobile friendly for ALL screen sizes!
+	// TODO - prevent the user from being redirected if they hit the like, comment, settings, etc. buttons on the post
+	// TODO - add the '/post/post id' route
+	// TODO - style the 'loading...' text
 	return (
 		<div className="bg-black w-full text-white min-h-screen overflow-auto">
 			<div className="bg-black h-screen">
@@ -37,6 +51,18 @@ export default function Landing() {
 				<div>
 					<Editor />
 				</div>
+				{loadingStatus
+					? "Loading..."
+					: postData.map((post: IPost) => {
+							return (
+								<Link to={`/post/${post._id}`}>
+									<Post
+										isOwner={post.user._id === userData?._id}
+										postData={post}
+									/>
+								</Link>
+							);
+					  })}
 			</div>
 		</div>
 	);
