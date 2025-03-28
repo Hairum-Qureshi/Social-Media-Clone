a2af5eacb22bb6f3d3e339b9a1f00d80a81b2bed
Hairum Qureshi <hqureshi@udel.edu>
2025-03-28 08:21:25 -0400

    utilized 'e.stopPropogation()' and 'e.preventDefault()' to prevent redirect upon clicking buttons on posts and utilized moment to format the posts' createdAt date so it displays in a better format

--- frontend/src/components/pages/feed/editor-tools/Post.tsx
+++ frontend/src/components/pages/feed/editor-tools/Post.tsx
@@ -7,6 +7,8 @@ import { faRetweet } from "@fortawesome/free-solid-svg-icons/faRetweet";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons/faBookmark";
 import Carousel from "./carousel/Carousel";
 import { PostProps } from "../../../../interfaces";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
 export default function Post({ isOwner, postData }: PostProps) {
 	const [showOptions, setShowOptions] = useState(false);
@@ -17,12 +19,18 @@ export default function Post({ isOwner, postData }: PostProps) {
 
 	// TODO - create a way so that for each Post component, only one can have the options open and any others close
 	// TODO - make sure to replace the hard-coded 'false' for the 'allowDelete' prop being passed into Carousel component
-	// TODO - replace hardcoded '7hr'
 	// TODO - need to add 'numReposts', 'numSaves' (to the Post database schema), and consider adding a number of shares too to be displayed
+	// TODO - make the redirect work for when the user clicks on a username, it should take the user to that user's profile page
 
 	return (
 		<div className="w-full p-2 border-t-2 border-b-2 border-t-gray-700 border-b-gray-700 relative">
-			{showOptions && <Options close={close} isOwner={isOwner} username = {postData.user.username} />}
+			{showOptions && (
+				<Options
+					close={close}
+					isOwner={isOwner}
+					username={postData.user.username}
+				/>
+			)}
 			<div className="flex">
 				<img
 					src={postData.user.profilePicture}
@@ -30,10 +38,20 @@ export default function Post({ isOwner, postData }: PostProps) {
 					className="lg:w-10 lg:h-10 w-12 h-12 rounded-full"
 				/>
 				<div className="lg:flex lg:flex-col lg:w-full">
-					<span className="text-base font-bold flex items-center ml-3">
-						{postData.user.fullName}&nbsp;
-						<span className="text-gray-500 font-light">@{postData.user.username} · 7hr</span>
-					</span>
+					<Link
+						to={`/${postData.user._id}`}
+						onClick={e => {
+							e.stopPropagation();
+							e.preventDefault();
+						}}
+					>
+						<span className="text-base font-bold flex items-center ml-3">
+							{postData.user.fullName}&nbsp;
+							<span className="text-gray-500 font-light">
+								@{postData.user.username} · {moment(postData.createdAt.toString()).fromNow()}
+							</span>
+						</span>
+					</Link>
 					{postData.text && <span className="ml-3">{postData.text}</span>}
 					<div>
 						{postData.postImages?.length > 0 && (
@@ -49,29 +67,63 @@ export default function Post({ isOwner, postData }: PostProps) {
 				</div>
 				<div
 					className="mr-3 hover:cursor-pointer"
-					onClick={() => setShowOptions(!showOptions)}
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+						setShowOptions(!showOptions);
+					}}
 				>
 					<FontAwesomeIcon icon={faEllipsis} />
 				</div>
 			</div>
 			<div className="grid grid-cols-5 gap-1 text-center text-gray-600 mt-3">
-				<div className="hover:cursor-pointer hover:text-sky-400">
+				<div
+					className="hover:cursor-pointer hover:text-sky-400"
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+					}}
+				>
 					<FontAwesomeIcon icon={faComment} />
 					<span className="ml-1">{postData.numComments}</span>
 				</div>
-				<div className="hover:cursor-pointer hover:text-green-400">
+				<div
+					className="hover:cursor-pointer hover:text-green-400"
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+					}}
+				>
 					<FontAwesomeIcon icon={faRetweet} />
 					<span className="ml-1">0</span>
 				</div>
-				<div className="hover:cursor-pointer hover:text-red-500">
+				<div
+					className="hover:cursor-pointer hover:text-red-500"
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+					}}
+				>
 					<FontAwesomeIcon icon={faHeart} />
 					<span className="ml-1">{postData.numLikes}</span>
 				</div>
-				<div className="hover:cursor-pointer hover:text-yellow-400">
+				<div
+					className="hover:cursor-pointer hover:text-yellow-400"
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+					}}
+				>
 					<FontAwesomeIcon icon={faBookmark} />
 					<span className="ml-1">0</span>
 				</div>
-				<div className="hover:cursor-pointer hover:text-sky-400">
+				<div
+					className="hover:cursor-pointer hover:text-sky-400"
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+					}}
+				>
 					<FontAwesomeIcon icon={faShare} />
 				</div>
 			</div>
