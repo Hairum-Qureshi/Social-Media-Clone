943ec9ceffa5fdca8d6b7be474b07e28e9315c85
Hairum Qureshi <hqureshi@udel.edu>
2025-03-31 13:49:43 -0400

    added multer middleware to the '/create' route POST request to handle image uploads and imported storage config for multer

--- api/routes/post.ts
+++ api/routes/post.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { createPost, deletePost, handleLikes, postComment, getAllPosts, getAllLikedPosts, getFollowingUsersPosts, getUserPosts, deleteComment, getAllYourPosts } from "../controllers/post";
+import {
+	createPost,
+	deletePost,
+	handleLikes,
+	postComment,
+	getAllPosts,
+	getAllLikedPosts,
+	getFollowingUsersPosts,
+	getUserPosts,
+	deleteComment,
+	getAllCurrUserPosts
+} from "../controllers/post";
+import multer from "multer";
+import storage from "../config/multer-config";
 
+const upload = multer({ storage });
 const router = express.Router();
 
-router.post("/create", checkAuthStatus, createPost);
+router.post("/create", checkAuthStatus, upload.array("images", 4), createPost);
 router.delete("/:postID", checkAuthStatus, deletePost);
 router.post("/handle-likes/:postID", checkAuthStatus, handleLikes);
 router.post("/comment/:postID", checkAuthStatus, postComment);
@@ -12,7 +26,7 @@ router.get("/all", checkAuthStatus, getAllPosts);
 router.get("/liked-posts/:userID", checkAuthStatus, getAllLikedPosts);
 router.get("/following", checkAuthStatus, getFollowingUsersPosts);
 router.get("/user/:username", checkAuthStatus, getUserPosts);
-router.get("/current-user/all", checkAuthStatus, getAllYourPosts);
+router.get("/current-user/all", checkAuthStatus, getAllCurrUserPosts);
 router.delete("/comment/:commentID/:postID", checkAuthStatus, deleteComment);
 
 export default router;
