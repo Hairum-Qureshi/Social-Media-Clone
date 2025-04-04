7ce63565e1ecbe697fd82e8e53382be6e00a7423
Hairum Qureshi <hqureshi@udel.edu>
2025-04-03 23:09:50 -0400

    added new 'forPost' prop and added logic to render out the close button on the image if 'allowDelete' is true and 'forPost' is false

--- frontend/src/components/pages/feed/editor-tools/carousel/CarouselImage.tsx
+++ frontend/src/components/pages/feed/editor-tools/carousel/CarouselImage.tsx
@@ -6,11 +6,12 @@ export default function CarouselImage({
 	image,
 	imageIndex,
 	removeImage,
-	allowDelete
+	allowDelete,
+	forPost
 }: CarouselImageProps) {
 	return (
 		<div className="w-full h-full m-2 relative">
-			{allowDelete && <div
+			{allowDelete && !forPost && <div
 				className="absolute top-0 right-0 text-lg text-white p-2 bg-black rounded-full flex items-center justify-center h-8 w-8 m-2 hover:cursor-pointer"
 				onClick={() => removeImage(imageIndex)}
 			>
