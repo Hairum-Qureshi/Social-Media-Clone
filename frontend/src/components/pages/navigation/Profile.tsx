f8cedbb60d5321bf1c4eef7fe81d99c274828244
Hairum Qureshi <hqureshi@udel.edu>
2025-04-04 12:04:05 -0400

    added logic to render out a verification badge next to the user's username if the user has a verified account

--- frontend/src/components/pages/navigation/Profile.tsx
+++ frontend/src/components/pages/navigation/Profile.tsx
@@ -1,23 +1,40 @@
 import {
 	faArrowLeft,
 	faCalendarDays,
+	faCertificate,
 	faLock,
 	faPencil
 } from "@fortawesome/free-solid-svg-icons";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Post from "../feed/editor-tools/Post";
 import { useRef, useState } from "react";
 import UserSettingsModal from "../../UserSettingsModal";
 import useAuthContext from "../../../contexts/AuthContext";
+import usePosts from "../../../hooks/usePosts";
+import { Link } from "react-router-dom";
+import { Post as IPost } from "../../../interfaces";
+import Post from "../feed/editor-tools/Post";
 
 // ! FIX: the kebab buttons are not shifted to the right in mobile view for the posts
 export default function Profile() {
 	const [showModal, setShowModal] = useState(false);
 	const [isPosts, setIsPosts] = useState(true);
 	const [isLikes, setIsLikes] = useState(false);
-	const fileInputRef = useRef<HTMLInputElement>(null);
+	const filePFPInputRef = useRef<HTMLInputElement>(null);
+	const fileBackgroundImageInputRef = useRef<HTMLInputElement>(null);
 	const { userData } = useAuthContext()!;
+	const { currentUserPostData } = usePosts();
+	// const [pfpBlob, setPfpBlob] = useState("");
+	// const [backgroundBlob, setBackgroundBlob] = useState("");
+
+	function handleImage(event: React.ChangeEvent<HTMLInputElement>) {
+		const files: FileList | null = event.target.files;
+		if (files && files.length > 0) {
+			const blobUrl = window.URL.createObjectURL(files[0]);
+			// setUploadedImages(prev => [...prev, blobUrl]);
+			console.log("Image uploaded:", blobUrl);
+		}
+	}
 
 	function closeModal() {
 		setShowModal(false);
@@ -49,11 +66,17 @@ export default function Profile() {
 					src={userData?.coverImage}
 					alt="Background image"
 					className="w-full h-full object-cover"
-					accept="image/png, image/gif, image/jpeg"
+				/>
+				<input
+					type="file"
+					onChange={event => handleImage(event)}
+					className="hidden"
+					ref={fileBackgroundImageInputRef}
+					accept="image/*"
 				/>
 				<div
 					className="absolute top-0 right-0 mr-5 bg-slate-500 rounded-lg p-2 border-2 border-gray-300 w-8 h-8 text-sm flex items-center justify-center opacity-40 hover:opacity-100 hover:cursor-pointer active:opacity-80"
-					onClick={() => fileInputRef.current?.click()}
+					onClick={() => fileBackgroundImageInputRef.current?.click()}
 				>
 					<FontAwesomeIcon icon={faPencil} />
 				</div>
@@ -62,11 +85,16 @@ export default function Profile() {
 				<img
 					src={userData?.profilePicture}
 					alt="User profile picture"
-					accept="image/png, image/gif, image/jpeg"
 					className="border-2 border-black rounded-full z-10 w-1/4 h-1/4 object-cover ml-10 lg:-mt-28 -mt-20 hover:opacity-85 active:opacity-80"
-					onClick={() => fileInputRef.current?.click()}
+					onClick={() => filePFPInputRef.current?.click()}
 				/>
-				<input type="file" ref={fileInputRef} className="hidden" />
+				<input
+					type="file"
+					onChange={event => handleImage(event)}
+					className="hidden"
+					accept="image/*"
+					ref={filePFPInputRef}
+				/>
 				<button
 					className="ml-auto mr-10 border-2 border-white text-sm rounded-full p-2"
 					onClick={() => setShowModal(true)}
@@ -74,16 +102,24 @@ export default function Profile() {
 					Edit Profile
 				</button>
 			</div>
+
 			<div className="w-full border-b-2 border-b-gray-700">
 				<div className="mt-5">
-					<h3 className="font-bold ml-5 text-xl">{userData?.fullName}</h3>
+					<h3 className="font-bold ml-5 text-xl">
+						{userData?.fullName}{" "}
+						{userData?.isVerified && (
+							<span className="text-purple-500" title="This is a verified account">
+								<FontAwesomeIcon icon={faCertificate} />
+							</span>
+						)}
+					</h3>
 					<p className="text-gray-500 text-base ml-5">@{userData?.username}</p>
 					<div className="mx-5 my-2">
 						<div>{userData?.bio}</div>
 						<div className="mt-3 text-gray-500">
 							<span>
 								<FontAwesomeIcon icon={faLocationDot} />
-								&nbsp; United States
+								&nbsp; {userData?.location || "N/A"}
 							</span>
 							<span className="ml-5">
 								<FontAwesomeIcon icon={faCalendarDays} />
@@ -136,11 +172,19 @@ export default function Profile() {
 			</div>
 			<div>
 				{isPosts ? (
-					<>
-						<Post text={"Hi"} />
-						<Post text={"Another post"} />
-						<Post text={"Another post!"} />
-					</>
+					currentUserPostData?.length > 0 ? (
+						currentUserPostData?.map((post: IPost) => {
+							return (
+								<Link to={`/post/${post._id}`} key={post._id}>
+									<Post isOwner={true} postData={post} />
+								</Link>
+							);
+						})
+					) : (
+						<p className="text-slate-400 text-center mt-10 font-semibold">
+							Make your first post!
+						</p>
+					)
 				) : (
 					<div className="bg-blue-950 rounded-md m-2 text-white p-2 text-sm">
 						<p>
