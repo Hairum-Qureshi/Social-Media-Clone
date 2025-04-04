8ef07b701cf960417922ce5714deae3e909d8598
Hairum Qureshi <hqureshi@udel.edu>
2025-04-04 10:55:09 -0400

    created new update profile route specifically for handling the pfp and backdrop images

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,12 +1,19 @@
 import express from "express";
-import { getProfile, getSuggestedUsers, handleFollowStatus, updateProfile } from "../controllers/user";
+import {
+	getProfile,
+	getSuggestedUsers,
+	handleFollowStatus,
+	updateProfile
+} from "../controllers/user";
 import checkAuthStatus from "../middleware/checkAuthStatus";
+import { upload } from "./post";
 
 const router = express.Router();
 
-router.get('/profile/:username', checkAuthStatus, getProfile);
-router.get('/suggested', checkAuthStatus, getSuggestedUsers);
-router.post('/follow-status/:uid', checkAuthStatus, handleFollowStatus);
-router.post('/update-profile', checkAuthStatus, updateProfile);
+router.get("/profile/:username", checkAuthStatus, getProfile);
+router.get("/suggested", checkAuthStatus, getSuggestedUsers);
+router.post("/follow-status/:uid", checkAuthStatus, handleFollowStatus);
+router.post("/update-profile", checkAuthStatus, updateProfile);
+router.post("/update-profile/images", checkAuthStatus, upload.single("image"), updateProfile);
 
 export default router;
