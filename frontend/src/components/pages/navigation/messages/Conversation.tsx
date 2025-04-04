5dd3ed680668b0833f720969764de6aa92468127
Hairum Qureshi <hqureshi@udel.edu>
2025-04-04 12:53:39 -0400

    created UI for the conversation side div

--- frontend/src/components/pages/navigation/messages/Conversation.tsx
+++ frontend/src/components/pages/navigation/messages/Conversation.tsx
@@ -1,3 +1,16 @@
 export default function Conversation() {
-	return <div>Conversation</div>;
+	return (
+		<div className="flex h-screen">
+			<div className="m-auto w-2/3">
+				<h2 className="font-bold text-4xl">Select a message</h2>
+				<p className="text-slate-500 mt-3">
+					Choose from your existing conversations, start a new one, or just keep
+					swimming.
+				</p>
+				<button className="bg-sky-500 px-8 py-4 text-lg text-white rounded-full mt-5 font-semibold">
+					New message
+				</button>
+			</div>
+		</div>
+	);
 }
