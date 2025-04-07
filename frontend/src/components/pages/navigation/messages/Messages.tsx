4b38d1dc0ada5c7f7fa1ba3bcab5e729431ebcd4
Hairum Qureshi <hqureshi@udel.edu>
2025-04-07 11:56:13 -0400

    allowed props to be accepted into Conversation component so the message displayed in the component is dynamic

--- frontend/src/components/pages/navigation/messages/Messages.tsx
+++ frontend/src/components/pages/navigation/messages/Messages.tsx
@@ -3,12 +3,16 @@ import Conversation from "./Conversation";
 
 export default function Messages() {
 	return (
-		<div className = "bg-black inline-flex w-full">
-			<div className="border border-slate-600 w-2/5">
+		<div className="bg-black inline-flex w-full">
+			<div className="border border-slate-600 w-5/12">
 				<Contacts />
 			</div>
-			<div className = "text-white border border-slate-600 flex-grow">
-				<Conversation />
+			<div className="text-white border border-slate-600 flex-grow">
+				<Conversation
+					defaultSubtext={
+						"Choose from your existing conversations, start a new one, or just keep swimming."
+					}
+				/>
 			</div>
 		</div>
 	);
