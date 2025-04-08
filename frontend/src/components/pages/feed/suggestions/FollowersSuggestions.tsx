061a664e584955a2a9f04b9971e33eb48df45095
Hairum Qureshi <hqureshi@udel.edu>
2025-04-08 18:54:49 -0400

    added 'showFollowStatus' props to the UserCard component

--- frontend/src/components/pages/feed/suggestions/FollowersSuggestions.tsx
+++ frontend/src/components/pages/feed/suggestions/FollowersSuggestions.tsx
@@ -4,11 +4,11 @@ export default function FollowersSuggestions() {
 	return (
 		<div className="rounded-md bg-zinc-900 my-5 mx-3 p-2 hidden lg:block">
 			<h1 className="font-bold text-xl m-3">Who to follow</h1>
-			<UserCard />
-			<UserCard />
-			<UserCard />
-			<UserCard />
-			<UserCard />
+			<UserCard showFollowStatus={false} />
+			<UserCard showFollowStatus={false} />
+			<UserCard showFollowStatus={false} />
+			<UserCard showFollowStatus={false} />
+			<UserCard showFollowStatus={false} />
 		</div>
 	);
 }
