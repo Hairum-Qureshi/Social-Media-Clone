8e95273585de4079eb64f149d24e101f6c41d047
Hairum Qureshi <hqureshi@udel.edu>
2025-04-08 15:55:25 -0400

    added logic to render out the modal if the URL contains '/compose' or if the user presses the 'write a message' button

--- frontend/src/components/pages/navigation/messages/Contacts.tsx
+++ frontend/src/components/pages/navigation/messages/Contacts.tsx
@@ -1,30 +1,58 @@
 import { faEnvelope, faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import UserSearchModal from "./UserSearchModal";
+import { useState } from "react";
 
 export default function Contacts() {
+	const path = window.location.pathname;
+	const [openModal, setOpenModal] = useState(false);
+
 	return (
-		<div>
-			<div className="flex p-2">
-				<h3 className="font-semibold w-full text-white text-xl">Messages</h3>
-				<div className="text-white inline-flex items-center text-lg">
-					<span className="mr-3">
-						<FontAwesomeIcon icon={faGear} />
-					</span>
-					<span className="mr-3">
-						<FontAwesomeIcon icon={faEnvelope} />
-					</span>
+		<>
+			{(path.includes("/compose") || openModal) && <UserSearchModal />}
+			<div>
+				<div className="flex p-2">
+					<h3 className="font-semibold w-full text-white text-xl m-3">
+						Messages
+					</h3>
+					<div className="text-white inline-flex items-center text-lg">
+						<span className="mr-3">
+							<FontAwesomeIcon icon={faGear} />
+						</span>
+						<span className="mr-3">
+							<FontAwesomeIcon icon={faEnvelope} />
+						</span>
+					</div>
+				</div>
+				<Link to="/messages/requests">
+					<div className="text-white flex w-full hover:bg-zinc-900 hover:cursor-pointer p-3">
+						<div className="">
+							<span className="mr-3 text-lg border-2 border-slate-700 rounded-full w-12 h-12 flex items-center justify-center">
+								<FontAwesomeIcon icon={faEnvelope} />
+							</span>
+						</div>
+						<p className="flex items-center">Message Requests</p>
+					</div>
+				</Link>
+				<div className="text-white">
+					<div className="m-10">
+						<h2 className="text-4xl font-bold">Welcome to your inbox!</h2>
+						<p className="text-slate-500 mt-3">
+							Drop a line, share posts and more with private conversations
+							between you and others on X.
+						</p>
+						<Link to="/messages/compose">
+							<button
+								className="bg-sky-500 px-8 py-4 text-lg text-white rounded-full mt-5 font-semibold"
+								onClick={() => setOpenModal(true)}
+							>
+								Write a message
+							</button>
+						</Link>
+					</div>
 				</div>
 			</div>
-			<div className="text-white">
-				<div className="m-10">
-					<h2 className="text-4xl font-bold">Welcome to your inbox!</h2>
-					<p className="text-slate-500 mt-3">
-						Drop a line, share posts and more with private conversations between
-						you and others on X.
-					</p>
-                    <button className = "bg-sky-500 px-8 py-4 text-lg text-white rounded-full mt-5 font-semibold">Write a message</button>
-				</div>
-			</div>
-		</div>
+		</>
 	);
 }
