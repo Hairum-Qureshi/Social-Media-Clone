3f2a3b9ca04835bed9966222c6b679ef5a07d55a
Hairum Qureshi <hqureshi@udel.edu>
2025-04-11 22:40:54 -0400

    updated props to take in user data as well and replaced all hard coded data so it's now dynamic per user

--- frontend/src/components/pages/feed/UserCard.tsx
+++ frontend/src/components/pages/feed/UserCard.tsx
@@ -5,22 +5,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function UserCard({
 	showFollowButton = true,
 	isFollowing = false,
-	showFollowStatus
+	showFollowStatus,
+	userData
 }: UserCardProps) {
 	// TODO - make sure to render out the verified badges too if the user is verified
 	return (
 		<div className="p-2">
 			<div className="flex items-center justify-center">
 				<img
-					src="https://i.pinimg.com/474x/2c/47/d5/2c47d5dd5b532f83bb55c4cd6f5bd1ef.jpg"
+					src={userData.profilePicture}
 					alt="User pfp"
 					className="lg:w-10 lg:h-10 w-12 h-12 rounded-full"
 				/>
 				<div className="hidden lg:flex lg:flex-col lg:w-full lg:text-left lg:ml-3">
 					<span className="text-base font-bold flex justify-between items-center">
-						Username
+						{userData.fullName}
 					</span>
-					<span className="text-sm text-gray-500 font-light">@username</span>
+					<span className="text-sm text-gray-500 font-light">
+						@{userData.username}
+					</span>
 					{showFollowStatus
 						? isFollowing && (
 								<span className="text-gray-500 text-xs">
