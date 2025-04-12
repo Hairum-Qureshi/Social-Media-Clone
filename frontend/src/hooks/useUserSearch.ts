1d06f8223bb651b833af43f47ecfde9cb83e1c4d
Hairum Qureshi <hqureshi@udel.edu>
2025-04-11 22:42:01 -0400

    removed old handler function for creating tags and updated logic for adding tags so now whatever user the user clicks on, it'll create a tag

--- frontend/src/hooks/useUserSearch.ts
+++ frontend/src/hooks/useUserSearch.ts
@@ -1,37 +1,58 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react";
-import { UserData, UserSearchTools } from "../interfaces";
+import { UserData, UserSearchTools, UserTagData } from "../interfaces";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 export default function useUserSearch(): UserSearchTools {
 	const [searchedUser, setSearchedUser] = useState("");
-	const [searchedUsers, setSearchedUsers] = useState<string[]>([]);
+	const [searchedUsers, setSearchedUsers] = useState<UserTagData[]>([]);
 	const [searching, setSearching] = useState(false);
-    const [returnedUsers, setReturnedUsers] = useState<UserData[]>([]); 
+	const [returnedUsers, setReturnedUsers] = useState<UserData[]>([]);
 	const keyUpTimer = useRef<number | null>(null);
 	const path: string = window.location.pathname;
 
 	function deleteUser(tagIndex: number) {
-		const filteredSearchedUsers: string[] = searchedUsers.filter(
+		const filteredSearchedUsers: UserTagData[] = searchedUsers.filter(
 			(_, index: number) => tagIndex !== index
 		);
 
 		setSearchedUsers(filteredSearchedUsers);
 	}
 
-	function handleUserTag(e: KeyboardEvent) {
+	function addUserTag(user: UserData) {
 		// TODO you *might* want to add a character limit restriction here
-		if (e.key === "Enter" && searchedUser.trim()) {
+
+		if (searchedUser.trim()) {
 			if (!path.includes("/group")) {
 				if (searchedUsers.length > 0) return;
-				else setSearchedUsers([searchedUser]);
-			} else {
-				const found: number = searchedUsers.indexOf(searchedUser);
-				if (found === -1 && searchedUsers.length <= 256)
-					setSearchedUsers([...searchedUsers, searchedUser]);
-				else return;
-			}
+				else
+					setSearchedUsers([
+						{
+							pfp: user.profilePicture,
+							fullName: user.fullName
+						}
+					]);
 
+				const filterUsers = returnedUsers.filter(
+					(returnedUser: UserData) => returnedUser._id !== user._id
+				);
+
+				setReturnedUsers(filterUsers);
+			} else {
+				const found: UserTagData | undefined = searchedUsers.find(
+					(u: UserTagData) => u.fullName === user.fullName
+				);
+
+				if (!found && searchedUsers.length < 256) {
+					setSearchedUsers([
+						...searchedUsers,
+						{
+							pfp: user.profilePicture,
+							fullName: user.fullName
+						}
+					]);
+				}
+			}
 			setSearchedUser("");
 		}
 	}
@@ -63,7 +84,7 @@ export default function useUserSearch(): UserSearchTools {
 					}
 				);
 
-                setReturnedUsers(response.data);
+				setReturnedUsers(response.data);
 				return response.data;
 			} catch (error) {
 				console.error("Error posting:", error);
@@ -90,13 +111,13 @@ export default function useUserSearch(): UserSearchTools {
 
 	return {
 		deleteUser,
-		handleUserTag,
 		searchedUser,
 		searchedUsers,
 		path,
 		updateSearchedUser,
 		autoSearch,
 		searching,
-        returnedUsers
+		returnedUsers,
+		addUserTag
 	};
 }
