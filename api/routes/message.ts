e882b4d67ffe5659bd43da06d0ad2f4b6193237e
Hairum Qureshi <hqureshi@udel.edu>
2025-04-14 11:08:38 -0400

    created route to get all conversations

--- api/routes/message.ts
+++ api/routes/message.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { getSearchedUsers } from "../controllers/message";
+import { getAllConversations, getSearchedUsers } from "../controllers/message";
 
 const router = express.Router();
 
 router.post("/searched-users", checkAuthStatus, getSearchedUsers);
+router.get("/conversations", checkAuthStatus, getAllConversations);
 
 export default router;
