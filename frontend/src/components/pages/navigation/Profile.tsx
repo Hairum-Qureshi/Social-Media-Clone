7bc7aa9228671f1b27e98169c9c43547c7dc610f
Hairum Qureshi <hqureshi@udel.edu>
2025-04-14 14:16:45 -0400

    added logic to render certain things on the profile page if the current user is only on their profile page and not any other user's page and implemented logic so that based on what profile the user visits, it displays that user's data on it

--- frontend/src/components/pages/navigation/Profile.tsx
+++ frontend/src/components/pages/navigation/Profile.tsx
@@ -14,7 +14,11 @@ import usePosts from "../../../hooks/usePosts";
 import { Link } from "react-router-dom";
 import { Post as IPost } from "../../../interfaces";
 import Post from "../feed/editor-tools/Post";
+import useProfile from "../../../hooks/useProfile";
 
+// TODO - get the number of posts the user has (currently it only works for the current user and not other users)
+// TODO - need to get other users' posts too to display on their profile pages
+// TODO - add a kebab button to allow users to block profiles too
 // ! FIX: the kebab buttons are not shifted to the right in mobile view for the posts
 export default function Profile() {
 	const [showModal, setShowModal] = useState(false);
@@ -24,6 +28,8 @@ export default function Profile() {
 	const fileBackgroundImageInputRef = useRef<HTMLInputElement>(null);
 	const { userData } = useAuthContext()!;
 	const { currentUserPostData } = usePosts();
+	const { profileData } = useProfile();
+
 	// const [pfpBlob, setPfpBlob] = useState("");
 	// const [backgroundBlob, setBackgroundBlob] = useState("");
 
@@ -57,88 +63,117 @@ export default function Profile() {
 					<FontAwesomeIcon icon={faArrowLeft} />
 				</div>
 				<div>
-					<h2 className="font-bold ml-5 text-xl">{userData?.username}</h2>
-					<div className="ml-5 text-gray-600">{currentUserPostData.length || 0} posts</div>
+					<h2 className="font-bold ml-5 text-xl">{profileData?.username}</h2>
+					<div className="ml-5 text-gray-600">
+						{currentUserPostData?.length || 0} posts
+					</div>
 				</div>
 			</div>
 			<div className="w-full h-1/4 relative">
 				<img
-					src={userData?.coverImage}
+					src={profileData?.coverImage}
 					alt="Background image"
 					className="w-full h-full object-cover"
 				/>
-				<input
-					type="file"
-					onChange={event => handleImage(event)}
-					className="hidden"
-					ref={fileBackgroundImageInputRef}
-					accept="image/*"
-				/>
-				<div
-					className="absolute top-0 right-0 mr-5 bg-slate-500 rounded-lg p-2 border-2 border-gray-300 w-8 h-8 text-sm flex items-center justify-center opacity-40 hover:opacity-100 hover:cursor-pointer active:opacity-80"
-					onClick={() => fileBackgroundImageInputRef.current?.click()}
-				>
-					<FontAwesomeIcon icon={faPencil} />
-				</div>
+				{profileData?._id === userData?._id && (
+					<input
+						type="file"
+						onChange={event => handleImage(event)}
+						className="hidden"
+						ref={fileBackgroundImageInputRef}
+						accept="image/*"
+					/>
+				)}
+				{profileData?._id === userData?._id && (
+					<div
+						className={`absolute top-0 right-0 bg-slate-500 rounded-lg p-2 border-2 border-gray-300 w-8 h-8 text-sm flex items-center justify-center m-3 ${
+							profileData?._id === userData?._id
+								? "opacity-40 hover:opacity-100 hover:cursor-pointer active:opacity-80"
+								: ""
+						}`}
+						onClick={() => fileBackgroundImageInputRef.current?.click()}
+					>
+						<FontAwesomeIcon icon={faPencil} />
+					</div>
+				)}
 			</div>
 			<div className="w-full flex items-center hover:cursor-pointer">
 				<img
-					src={userData?.profilePicture}
+					src={profileData?.profilePicture}
 					alt="User profile picture"
-					className="border-2 border-black rounded-full z-10 w-1/4 h-1/4 object-cover ml-10 lg:-mt-28 -mt-20 hover:opacity-85 active:opacity-80"
+					className={`border-2 border-black rounded-full z-10 w-1/4 h-1/4 object-cover ml-10 lg:-mt-28 -mt-20 ${
+						profileData?._id === userData?._id
+							? "hover:opacity-85 active:opacity-800"
+							: ""
+					}`}
 					onClick={() => filePFPInputRef.current?.click()}
 				/>
-				<input
-					type="file"
-					onChange={event => handleImage(event)}
-					className="hidden"
-					accept="image/*"
-					ref={filePFPInputRef}
-				/>
-				<button
-					className="ml-auto mr-10 border-2 border-white text-sm rounded-full p-2"
-					onClick={() => setShowModal(true)}
-				>
-					Edit Profile
-				</button>
+				{profileData?._id === userData?._id && (
+					<input
+						type="file"
+						onChange={event => handleImage(event)}
+						className="hidden"
+						accept="image/*"
+						ref={filePFPInputRef}
+					/>
+				)}
+				{profileData?._id === userData?._id ? (
+					<button
+						className="ml-auto mr-10 border-2 border-white text-sm rounded-full p-2"
+						onClick={() => setShowModal(true)}
+					>
+						Edit Profile
+					</button>
+				) : (
+					<>
+						<button className="ml-auto mr-10 border-2 bg-white text-black font-semibold w-20 text-base rounded-full p-2">
+							Follow
+						</button>
+					</>
+				)}
 			</div>
 
 			<div className="w-full border-b-2 border-b-gray-700">
 				<div className="mt-5">
 					<h3 className="font-bold ml-5 text-xl">
-						{userData?.fullName}{" "}
-						{userData?.isVerified && (
-							<span className="text-purple-500" title="This is a verified account">
+						{profileData?.fullName}{" "}
+						{profileData?.isVerified && (
+							<span
+								className="text-purple-500"
+								title="This is a verified account"
+							>
 								<FontAwesomeIcon icon={faCertificate} />
 							</span>
 						)}
 					</h3>
-					<p className="text-gray-500 text-base ml-5">@{userData?.username}</p>
+					<p className="text-gray-500 text-base ml-5">
+						@{profileData?.username}
+					</p>
 					<div className="mx-5 my-2">
-						<div>{userData?.bio}</div>
+						<div>{profileData?.bio}</div>
 						<div className="mt-3 text-gray-500">
 							<span>
 								<FontAwesomeIcon icon={faLocationDot} />
-								&nbsp; {userData?.location || "N/A"}
+								&nbsp; {profileData?.location || "N/A"}
 							</span>
 							<span className="ml-5">
 								<FontAwesomeIcon icon={faCalendarDays} />
 								&nbsp; Joined{" "}
-								{`${getMonthAndYear(userData?.createdAt)?.month} ${
-									getMonthAndYear(userData?.createdAt)?.year
+								{`${getMonthAndYear(profileData?.createdAt)?.month} ${
+									getMonthAndYear(profileData?.createdAt)?.year
 								}`}
 							</span>
 						</div>
 						<div className="w-full mt-3 flex mb-3">
 							<p className="text-gray-500">
 								<span className="text-white font-bold">
-									{userData?.numFollowers}
+									{profileData?.numFollowers}
 								</span>
 								<span className="">&nbsp; Followers</span>
 							</p>
 							<p className="text-gray-500 ml-3">
 								<span className="text-white font-bold">
-									{userData?.numFollowing}
+									{profileData?.numFollowing}
 								</span>
 								<span className="">&nbsp; Following</span>
 							</p>
@@ -165,7 +200,7 @@ export default function Profile() {
 								setIsPosts(false);
 							}}
 						>
-							Likes
+							{profileData?._id === userData?._id ? "Likes" : "Media"}
 						</div>
 					</div>
 				</div>
@@ -185,7 +220,7 @@ export default function Profile() {
 							Make your first post!
 						</p>
 					)
-				) : (
+				) : profileData?._id === userData?._id ? (
 					<div className="bg-blue-950 rounded-md m-2 text-white p-2 text-sm">
 						<p>
 							<span className="mx-3">
@@ -193,6 +228,10 @@ export default function Profile() {
 							</span>
 							Your likes are private. Only you can see them.
 						</p>
+					</div>
+				) : (
+					<div className="bg-blue-950 rounded-md m-2 text-white p-2 text-sm">
+						<p>Media Content</p>
 					</div>
 				)}
 				{/* Show the follower suggestions IF the user hasn't posted anything */}
