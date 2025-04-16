035848f717a1bbc8bd36112ae78d7512352f2c34
Hairum Qureshi <hqureshi@udel.edu>
2025-04-16 15:15:03 -0400

    added new prop 'showHeaderText' that will render the header text if the user hasn't clicked on a conversation

--- frontend/src/components/pages/navigation/messages/Conversation.tsx
+++ frontend/src/components/pages/navigation/messages/Conversation.tsx
@@ -1,15 +1,21 @@
-export default function Conversation() {
+interface ConversationProps {
+	defaultSubtext: string;
+	showHeaderText: boolean;
+}
+
+export default function Conversation({
+	defaultSubtext,
+	showHeaderText = true
+}: ConversationProps) {
 	return (
 		<div className="flex h-screen">
 			<div className="m-auto w-2/3">
-				<h2 className="font-bold text-4xl">Select a message</h2>
-				<p className="text-slate-500 mt-3">
-					Choose from your existing conversations, start a new one, or just keep
-					swimming.
-				</p>
-				<button className="bg-sky-500 px-8 py-4 text-lg text-white rounded-full mt-5 font-semibold">
-					New message
-				</button>
+				{showHeaderText && (
+					<h2 className="font-bold text-4xl">Select a message</h2>
+				)}
+				{defaultSubtext && (
+					<p className="text-zinc-500 mt-3">{defaultSubtext}</p>
+				)}
 			</div>
 		</div>
 	);
