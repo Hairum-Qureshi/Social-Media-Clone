52aa2ba2710588342f66b95a7ea859cd977213c8
Hairum Qureshi <hqureshi@udel.edu>
2025-04-16 15:16:17 -0400

    added function to set the selected conversation and save it in state

--- frontend/src/components/pages/navigation/messages/Messages.tsx
+++ frontend/src/components/pages/navigation/messages/Messages.tsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import Contacts from "./Contacts";
 import Conversation from "./Conversation";
+import { Conversation as IConversation } from "../../../../interfaces";
 
 export default function Messages() {
+	const [conversation, setConversation] = useState<IConversation>();
+
+	function setConvo(conversation: IConversation) {
+		setConversation(conversation);
+
+		console.log(conversation);
+	}
+
 	return (
 		<div className="bg-black inline-flex w-full">
 			<div className="border border-slate-600 w-5/12">
-				<Contacts />
+				<Contacts setConvo={setConvo} />
 			</div>
 			<div className="text-white border border-slate-600 flex-grow">
 				<Conversation
 					defaultSubtext={
-						"Choose from your existing conversations, start a new one, or just keep swimming."
-					}
+						!conversation ? "Choose from your existing conversations, start a new one, or just keep swimming." : ""
+					} 
+					showHeaderText = {!conversation ? true : false}
 				/>
 			</div>
 		</div>
