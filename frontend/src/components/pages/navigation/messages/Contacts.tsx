8f9c8b8e01adff51813e9ac4c0b657df4ce54164
Hairum Qureshi <hqureshi@udel.edu>
2025-04-16 02:28:44 -0400

    added logic for rendering out the user's conversations and made it so when you click on the envelope icon, it also displays the modal for creating a new conversation

--- frontend/src/components/pages/navigation/messages/Contacts.tsx
+++ frontend/src/components/pages/navigation/messages/Contacts.tsx
@@ -1,8 +1,14 @@
 import { faEnvelope, faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import UserSearchModal from "./UserSearchModal";
+import UserSearchModal from "./modal/UserSearchModal";
 import { useState } from "react";
+import useDM from "../../../../hooks/useDM";
+import DM from "./DM";
+import { Conversation, UserData_Conversation } from "../../../../interfaces";
+import useAuthContext from "../../../../contexts/AuthContext";
+
+// TODO - may need to add specific logic for displaying group chats
 
 export default function Contacts() {
 	const path = window.location.pathname;
@@ -13,6 +19,9 @@ export default function Contacts() {
 	function closeModal() {
 		setOpenModal(false);
 	}
+
+	const { conversations } = useDM();
+	const { userData } = useAuthContext()!;
 
 	return (
 		<>
@@ -26,7 +35,10 @@ export default function Contacts() {
 						<span className="mr-3">
 							<FontAwesomeIcon icon={faGear} />
 						</span>
-						<span className="mr-3">
+						<span
+							className="mr-3 hover:cursor-pointer"
+							onClick={() => setOpenModal(true)}
+						>
 							<FontAwesomeIcon icon={faEnvelope} />
 						</span>
 					</div>
@@ -42,21 +54,41 @@ export default function Contacts() {
 					</div>
 				</Link>
 				<div className="text-white">
-					<div className="m-10">
-						<h2 className="text-4xl font-bold">Welcome to your inbox!</h2>
-						<p className="text-slate-500 mt-3">
-							Drop a line, share posts and more with private conversations
-							between you and others on X.
-						</p>
-						<Link to="/messages/compose">
-							<button
-								className="bg-sky-500 px-8 py-4 text-lg text-white rounded-full mt-5 font-semibold"
-								onClick={() => setOpenModal(true)}
-							>
-								Write a message
-							</button>
-						</Link>
-					</div>
+					{!conversations || conversations.length === 0 ? (
+						<div className="m-10">
+							<h2 className="text-4xl font-bold">Welcome to your inbox!</h2>
+							<p className="text-slate-500 mt-3">
+								Drop a line, share posts and more with private conversations
+								between you and others on X.
+							</p>
+							<Link to="/messages/compose">
+								<button
+									className="bg-sky-500 px-8 py-4 text-lg text-white rounded-full mt-5 font-semibold"
+									onClick={() => setOpenModal(true)}
+								>
+									Write a message
+								</button>
+							</Link>
+						</div>
+					) : (
+						conversations?.map((conversation: Conversation) =>
+							conversation.users.map((user: UserData_Conversation) => {
+								if (user._id !== userData?._id) {
+									return (
+										<Link
+											to={`/messages/conversation/${conversation._id}/${userData?._id}-${user._id}`}
+										>
+											<DM
+												username={user.username}
+												pfp={user.profilePicture}
+												fullName={user.fullName}
+											/>
+										</Link>
+									);
+								}
+							})
+						)
+					)}
 				</div>
 			</div>
 		</>
