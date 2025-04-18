ce9c941e57a21a51d8326dd810f1660ac0e25e1a
Hairum Qureshi <hqureshi@udel.edu>
2025-04-17 22:21:33 -0400

    added logic to automatically reopen the chat if the URL contains that chat's ID

--- frontend/src/components/pages/navigation/messages/Messages.tsx
+++ frontend/src/components/pages/navigation/messages/Messages.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Contacts from "./Contacts";
 import Conversation from "./Conversation";
 import { Conversation as IConversation } from "../../../../interfaces";
+import useDM from "../../../../hooks/useDM";
 
 export default function Messages() {
 	const [conversation, setConversation] = useState<IConversation>();
+	const pathname: string[] = window.location.pathname.split("/");
 
 	function setConvo(conversation: IConversation) {
 		setConversation(conversation);
-
-		console.log(conversation);
 	}
+
+	const { conversations } = useDM();
+
+	useEffect(() => {
+		if (pathname.length === 5) {
+			const convoID = pathname[3];
+			const convo = conversations.find(
+				conversation => conversation._id === convoID
+			);
+			if (convo) {
+				setConvo(convo);
+			}
+		}
+	}, [pathname]);
 
 	return (
 		<div className="bg-black inline-flex w-full">
@@ -20,9 +34,12 @@ export default function Messages() {
 			<div className="text-white border border-slate-600 flex-grow">
 				<Conversation
 					defaultSubtext={
-						!conversation ? "Choose from your existing conversations, start a new one, or just keep swimming." : ""
-					} 
-					showHeaderText = {!conversation ? true : false}
+						!conversation
+							? "Choose from your existing conversations, start a new one, or just keep swimming."
+							: ""
+					}
+					showHeaderText={!conversation ? true : false}
+					conversation={conversation}
 				/>
 			</div>
 		</div>
