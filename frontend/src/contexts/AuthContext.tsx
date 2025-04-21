e7fc58293700ee548c1d6fa100aa542cb91eb0d7
Hairum Qureshi <hqureshi@udel.edu>
2025-04-20 20:25:30 -0400

    updated response.data code so that it accesses the userData property when retrieving the user data

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,56 @@
+import { useState, createContext, useContext, useEffect } from "react";
+import axios from "axios";
+import { AuthProps, ContextData, UserData } from "../interfaces";
+import { toast } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
+
+export const AuthContext = createContext<ContextData | null>(null);
+
+export const AuthProvider = ({ children }: AuthProps) => {
+	const [userData, setUserData] = useState<UserData | null>(null);
+
+	const { data } = useQuery({
+		queryKey: ["user"],
+		queryFn: async () => {
+			try {
+				const response = await axios.get(
+					`${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/current-user`,
+					{
+						withCredentials: true
+					}
+				);
+				return response.data.userData;
+			} catch (error) {
+				if (axios.isAxiosError(error)) {
+					toast(error.response?.data.error, {
+						autoClose: 500,
+						hideProgressBar: true
+					});
+				} else {
+					toast("An unknown error occurred", {
+						autoClose: 600,
+						hideProgressBar: true
+					});
+				}
+
+				return null;
+			}
+		}
+	});
+
+	useEffect(() => {
+		if (data) setUserData(data);
+	}, [data]);
+
+	const value: ContextData = {
+		userData
+	};
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuthContext = () => {
+	return useContext(AuthContext);
+};
+
+export default useAuthContext;
