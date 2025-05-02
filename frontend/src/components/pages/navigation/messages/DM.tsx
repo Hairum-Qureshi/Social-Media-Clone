70f58b31062fbcd5de92294dc7f953c721b02e7b
Hairum Qureshi <hqureshi@udel.edu>
2025-05-02 12:06:10 -0400

    utilized new 'conversationID' prop in conditional logic to only render out 'user is typing...' in the DM component in the appropriate chat

--- frontend/src/components/pages/navigation/messages/DM.tsx
+++ frontend/src/components/pages/navigation/messages/DM.tsx
@@ -1,13 +1,18 @@
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ContactProps } from "../../../../interfaces";
+import useSocketContext from "../../../../contexts/SocketIOContext";
 
 export default function DM({
+	conversationID,
 	username,
 	pfp,
 	fullName,
 	latestMessage
 }: ContactProps) {
+	const { typingIndicatorChatID, userIsTyping, typingUser } =
+		useSocketContext()!;
+
 	return (
 		<div className="w-full p-3 bg-zinc-800 border-r-2 border-r-sky-600 hover:bg-zinc-900 hover:cursor-pointer">
 			<div className="flex items-center">
@@ -26,9 +31,13 @@ export default function DM({
 						</div>
 					</div>
 					<div className="ml-3 text-gray-500">
-						{latestMessage.length > 40
-							? latestMessage.slice(0, 40) + "..."
-							: latestMessage}
+						{userIsTyping && typingIndicatorChatID === conversationID ? (
+							<span className="text-sky-600">@{typingUser} is typing...</span>
+						) : latestMessage.length > 40 ? (
+							latestMessage.slice(0, 40) + "..."
+						) : (
+							latestMessage
+						)}
 					</div>
 				</div>
 			</div>
