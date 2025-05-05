059de2159c60b50babc15c1e13a84c7398c54048
Hairum Qureshi <hqureshi@udel.edu>
2025-05-05 15:45:26 -0400

    replaced hardcoded data with dynamic data being retrieved from custom 'useNotifications' hook, added message that will be displayed if the user has no notifications, and updated props that get passed into child 'UserNotificationCard' component

--- frontend/src/components/pages/navigation/notification/Notifications.tsx
+++ frontend/src/components/pages/navigation/notification/Notifications.tsx
@@ -1,30 +1,36 @@
 import { faGear } from "@fortawesome/free-solid-svg-icons";
-import UserNotificationCard from "./UserNotificationCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useNotifications from "../../../../hooks/useNotifications";
+import { Notification } from "../../../../interfaces";
+import UserNotificationCard from "./UserNotificationCard";
+import { getDescription } from "../../../../utils/getDescription";
 
 export default function Notifications() {
+	const { notificationData } = useNotifications();
+
 	return (
 		<div className="bg-black w-full text-white min-h-screen overflow-auto">
-			<div className = "flex items-center">
+			<div className="flex items-center">
 				<h2 className="text-2xl font-bold m-5">Notifications</h2>
-				<span className = "text-2xl ml-auto m-5 hover:cursor-pointer"><FontAwesomeIcon icon={faGear} /></span>
+				<span className="text-2xl ml-auto m-5 hover:cursor-pointer">
+					<FontAwesomeIcon icon={faGear} />
+				</span>
 			</div>
-			<UserNotificationCard
-				notifType={"follow"}
-				notifDescription={"followed you"}
-			/>
-			<UserNotificationCard
-				notifType={"like"}
-				notifDescription={"liked your post"}
-			/>
-			<UserNotificationCard
-				notifType={"comment"}
-				notifDescription={"commented on your post"}
-			/>
-			<UserNotificationCard
-				notifType={"message"}
-				notifDescription={"sent you a message"}
-			/>
+			{!notificationData ? (
+				<h3 className="text-xl font-semibold text-center mt-10">
+					You currently don't have any notifications
+				</h3>
+			) : (
+				notificationData.map((notification: Notification) => (
+					<UserNotificationCard
+						username={notification.from.username}
+						userPfp={notification.from.profilePicture}
+						notifType={notification.notifType}
+						notifDescription={getDescription(notification.notifType)}
+						notifDate={notification.createdAt.toString()}
+					/>
+				))
+			)}
 		</div>
 	);
 }
