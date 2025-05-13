b299eaa77682aedfdd808e0ecc36c255acc69e69
Hairum Qureshi <hqureshi@udel.edu>
2025-05-13 16:46:38 -0400

    for routes handling updating, replaced 'post' with 'put'

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -4,7 +4,7 @@ import {
 	getSuggestedUsers,
 	handleFollowStatus,
 	updateProfile,
-	updateProfileImages
+	updateProfilePicture
 } from "../controllers/user";
 import checkAuthStatus from "../middleware/checkAuthStatus";
 import { upload } from "./post";
@@ -14,7 +14,8 @@ const router = express.Router();
 router.get("/profile/:username", checkAuthStatus, getProfile);
 router.get("/suggested", checkAuthStatus, getSuggestedUsers);
 router.post("/follow-status/:uid", checkAuthStatus, handleFollowStatus);
-router.post("/update-profile", checkAuthStatus, updateProfile);
-router.post("/update-profile/images", checkAuthStatus, upload.single("image"), updateProfileImages);
+router.put("/update-profile", checkAuthStatus, updateProfile);
+router.put("/update-profile/images/profile-picture", checkAuthStatus, upload.single("profile-picture"), updateProfilePicture);
+// router.put("/update-profile/images/backdrop", checkAuthStatus, upload.single("backdrop"), updateProfileBackdrop);
 
 export default router;
