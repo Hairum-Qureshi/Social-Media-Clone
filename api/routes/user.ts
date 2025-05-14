a5e78e2ee0935661351448a0a49f68f4212b81d0
Hairum Qureshi <hqureshi@udel.edu>
2025-05-14 11:47:04 -0400

    replaced 'put' with 'patch' requests

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -15,8 +15,8 @@ const router = express.Router();
 router.get("/profile/:username", checkAuthStatus, getProfile);
 router.get("/suggested", checkAuthStatus, getSuggestedUsers);
 router.post("/follow-status/:uid", checkAuthStatus, handleFollowStatus);
-router.put("/update-profile", checkAuthStatus, updateProfile);
-router.put("/update-profile/images/profile-picture", checkAuthStatus, upload.single("profile-picture"), updateProfilePicture);
-router.put("/update-profile/images/backdrop", checkAuthStatus, upload.single("backdrop"), updateProfileBackdrop);
+router.patch("/update-profile", checkAuthStatus, updateProfile);
+router.patch("/update-profile/images/profile-picture", checkAuthStatus, upload.single("profile-picture"), updateProfilePicture);
+router.patch("/update-profile/images/backdrop", checkAuthStatus, upload.single("backdrop"), updateProfileBackdrop);
 
 export default router;
