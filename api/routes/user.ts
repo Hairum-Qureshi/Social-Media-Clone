47e85b041be10f711de5f3ebd6a710f8a1cec3d6
Hairum Qureshi <hqureshi@udel.edu>
2025-05-14 08:45:01 -0400

    added route for updating the user's pfp backdrop image

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -4,6 +4,7 @@ import {
 	getSuggestedUsers,
 	handleFollowStatus,
 	updateProfile,
+	updateProfileBackdrop,
 	updateProfilePicture
 } from "../controllers/user";
 import checkAuthStatus from "../middleware/checkAuthStatus";
@@ -16,6 +17,6 @@ router.get("/suggested", checkAuthStatus, getSuggestedUsers);
 router.post("/follow-status/:uid", checkAuthStatus, handleFollowStatus);
 router.put("/update-profile", checkAuthStatus, updateProfile);
 router.put("/update-profile/images/profile-picture", checkAuthStatus, upload.single("profile-picture"), updateProfilePicture);
-// router.put("/update-profile/images/backdrop", checkAuthStatus, upload.single("backdrop"), updateProfileBackdrop);
+router.put("/update-profile/images/backdrop", checkAuthStatus, upload.single("backdrop"), updateProfileBackdrop);
 
 export default router;
