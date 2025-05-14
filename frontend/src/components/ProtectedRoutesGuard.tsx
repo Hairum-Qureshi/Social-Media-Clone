deb804d055ef9a0c73a0c00e22459a431a253edf
Hairum Qureshi <hqureshi@udel.edu>
2025-05-14 15:31:47 -0400

    modified and fixed logic for hiding the SideSuggestions component if the user is on the messages page and added logic for showing the follow suggestions Component on the side if the user has posts, or on the bottom if they haven't on their profile page

--- frontend/src/components/ProtectedRoutesGuard.tsx
+++ frontend/src/components/ProtectedRoutesGuard.tsx
@@ -1,15 +1,20 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import SideNavbar from "./pages/navigation/SideNavbar";
 import SideSuggestions from "./pages/feed/suggestions/SideSuggestions";
 import useAuthContext from "../contexts/AuthContext";
 import { useEffect, useState } from "react";
 import SignIn from "./pages/authentication/SignIn";
+import useProfile from "../hooks/useProfile";
+import usePosts from "../hooks/usePosts";
 
 export default function ProtectedRoutesGuard({
 	children
 }: React.PropsWithChildren) {
 	const { userData } = useAuthContext()!;
 	const [isLoading, setIsLoading] = useState(true);
+	const { profileData } = useProfile();
+	const { currentUserPostData } = usePosts();
+	const location = useLocation();
 
 	useEffect(() => {
 		if (userData !== null) {
@@ -19,13 +24,22 @@ export default function ProtectedRoutesGuard({
 
 	if (isLoading) return <SignIn />;
 
+	const isMessagesPage =
+		location.pathname.includes("/messages") ||
+		location.pathname.includes("/messages/requests");
+
+	const isProfilePage = location.pathname === `/${profileData?.username}`;
+	const showSideSuggestions =
+		isProfilePage && currentUserPostData.length === 0 ? false : true;
+
 	return userData ? (
 		<div className="w-full h-screen flex">
 			<SideNavbar />
 			<div className="flex-1 flex justify-center">{children}</div>
-			{(!window.location.href.includes("/messages") &&
-				!window.location.href.includes("/messages/requests")) && (
-				<SideSuggestions />
+			{!isMessagesPage && !isProfilePage ? (
+				<SideSuggestions showFollowerSuggestions={false} />
+			) : (
+				<SideSuggestions showFollowerSuggestions={showSideSuggestions} />
 			)}
 		</div>
 	) : (
