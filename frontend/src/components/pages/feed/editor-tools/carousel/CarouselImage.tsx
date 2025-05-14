2c89fb1de1d3a5b9713479db9db53ddb269bfc59
Hairum Qureshi <hqureshi@udel.edu>
2025-05-14 16:38:25 -0400

    modified component so that it takes in the numImage as a prop and utilizes it and modified CSS so that it handles styling uploaded images so that they're the same width and height

--- frontend/src/components/pages/feed/editor-tools/carousel/CarouselImage.tsx
+++ frontend/src/components/pages/feed/editor-tools/carousel/CarouselImage.tsx
@@ -7,20 +7,28 @@ export default function CarouselImage({
 	imageIndex,
 	removeImage,
 	allowDelete,
-	forPost
+	forPost,
+	numImages
 }: CarouselImageProps) {
 	return (
-		<div className="w-full h-full m-2 relative">
-			{allowDelete && !forPost && <div
-				className="absolute top-0 right-0 text-lg text-white p-2 bg-black rounded-full flex items-center justify-center h-8 w-8 m-2 hover:cursor-pointer"
-				onClick={() => removeImage(imageIndex)}
-			>
-				<FontAwesomeIcon icon={faX} />
-			</div>}
+		<div
+			className={`relative m-2 ${
+				numImages === 1 ? "w-full" : "w-1/2"
+			} aspect-square`}
+		>
+			{allowDelete && !forPost && (
+				<div
+					className="absolute top-0 right-0 text-lg text-white p-2 bg-black rounded-full flex items-center justify-center h-8 w-8 m-2 hover:cursor-pointer"
+					onClick={() => removeImage(imageIndex)}
+				>
+					<FontAwesomeIcon icon={faX} />
+				</div>
+			)}
+
 			<img
 				src={children as string}
-				className="object-contain rounded-md"
 				alt="Uploaded image"
+				className="w-full h-full object-cover rounded-md"
 			/>
 		</div>
 	);
