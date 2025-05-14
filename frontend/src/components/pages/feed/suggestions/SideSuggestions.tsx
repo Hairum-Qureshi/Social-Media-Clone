62812d81744dae5bd85c3f85b751f51a86c3a6ae
Hairum Qureshi <hqureshi@udel.edu>
2025-05-14 15:52:42 -0400

    removed console log statement

--- frontend/src/components/pages/feed/suggestions/SideSuggestions.tsx
+++ frontend/src/components/pages/feed/suggestions/SideSuggestions.tsx
@@ -1,9 +1,12 @@
+import { SideSuggestionsComponentProps } from "../../../../interfaces";
 import FollowersSuggestions from "./FollowersSuggestions";
 
-export default function SideSuggestions() {
+export default function SideSuggestions({
+	showFollowerSuggestions
+}: SideSuggestionsComponentProps) {
 	return (
 		<div className="lg:w-1/4 w-32 bg-black border-l-2 border-l-gray-700 overflow-hidden text-white">
-			<FollowersSuggestions />
+			{showFollowerSuggestions && <FollowersSuggestions />}
 		</div>
 	);
 }
