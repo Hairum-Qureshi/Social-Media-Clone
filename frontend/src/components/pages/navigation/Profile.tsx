45a831aab63d67aa80523271590442c887dd5933
Hairum Qureshi <hqureshi@udel.edu>
2025-05-14 20:21:35 -0400

    added code to display a 'media' tab as well and added conditional logic to get rid of the 'likes' tab if the current user visits another user's profile

--- frontend/src/components/pages/navigation/Profile.tsx
+++ frontend/src/components/pages/navigation/Profile.tsx
@@ -17,37 +17,37 @@ import Post from "../feed/editor-tools/Post";
 import useProfile from "../../../hooks/useProfile";
 import isFollowing from "../../../utils/checkFollowingStatus";
 import getMonthAndYear from "../../../utils/getMonthAndYear";
+import FollowersSuggestions from "../feed/suggestions/FollowersSuggestions";
 
 // TODO - get the number of posts the user has (currently it only works for the current user and not other users)
 // TODO - need to get other users' posts too to display on their profile pages
 // TODO - add a kebab button to allow users to block profiles too
 // TODO - if the user visits a profile of a non-existent user, it should lead to a 404 page
+// TODO - need to display some kind of loading indication to show that the profile picture/backdrop is being uploaded
+// TODO - make the back button work
+// TODO - sometimes when switching from one user profile to the other, it shows your profile first before showing the other user's
+// TODO - get rid of the lighter opacity effect when hovering over the pfp picture
+// TODO - change logic over looping over the 'currentUserPosts' so that when you go to a different user's page, it displays their posts, not the current user's
+// TODO - add a tab for all post attachments
+// ! FIX: when updating the location in the settings modal, for some reason you still have to refresh the page to see the change
 // ! FIX: the kebab buttons are not shifted to the right in mobile view for the posts
 export default function Profile() {
 	const [showModal, setShowModal] = useState(false);
 	const [isPosts, setIsPosts] = useState(true);
+	const [isMedia, setIsMedia] = useState(false);
 	const [isLikes, setIsLikes] = useState(false);
 	const filePFPInputRef = useRef<HTMLInputElement>(null);
 	const fileBackgroundImageInputRef = useRef<HTMLInputElement>(null);
 	const { userData } = useAuthContext()!;
 	const { currentUserPostData } = usePosts();
 	const { profileData, handleFollowing } = useProfile();
-
-	// const [pfpBlob, setPfpBlob] = useState("");
-	// const [backgroundBlob, setBackgroundBlob] = useState("");
-
-	function handleImage(event: React.ChangeEvent<HTMLInputElement>) {
-		const files: FileList | null = event.target.files;
-		if (files && files.length > 0) {
-			const blobUrl = window.URL.createObjectURL(files[0]);
-			// setUploadedImages(prev => [...prev, blobUrl]);
-			console.log("Image uploaded:", blobUrl);
-		}
-	}
+	const [attachments, setAttachments] = useState(); // TEMPORARY
 
 	function closeModal() {
 		setShowModal(false);
 	}
+
+	const { handleImage } = useProfile();
 
 	return (
 		<div className="bg-black w-full text-white min-h-screen overflow-auto relative">
@@ -77,7 +77,7 @@ export default function Profile() {
 				{profileData?._id === userData?._id && (
 					<input
 						type="file"
-						onChange={event => handleImage(event)}
+						onChange={event => handleImage(event, false)}
 						className="hidden"
 						ref={fileBackgroundImageInputRef}
 						accept="image/*"
@@ -100,25 +100,27 @@ export default function Profile() {
 				<img
 					src={profileData?.profilePicture}
 					alt="User profile picture"
-					className={`border-2 border-black rounded-full z-10 w-1/4 h-1/4 object-cover ml-10 lg:-mt-28 -mt-20 ${
+					className={`border-2 border-black rounded-full z-10 object-cover w-40 h-40 ml-10 lg:-mt-28 -mt-20 ${
 						profileData?._id === userData?._id
-							? "hover:opacity-85 active:opacity-800"
+							? "hover:opacity-85 active:opacity-80"
 							: ""
 					}`}
 					onClick={() => filePFPInputRef.current?.click()}
 				/>
+
 				{profileData?._id === userData?._id && (
 					<input
 						type="file"
-						onChange={event => handleImage(event)}
+						onChange={event => handleImage(event, true)}
 						className="hidden"
 						accept="image/*"
 						ref={filePFPInputRef}
 					/>
 				)}
+
 				{profileData?._id === userData?._id ? (
 					<button
-						className="ml-auto mr-10 border-2 border-white text-sm rounded-full p-2"
+						className="ml-auto mr-10 mt-5 border-2 border-white text-sm rounded-full p-2"
 						onClick={() => setShowModal(true)}
 					>
 						Edit Profile
@@ -143,9 +145,8 @@ export default function Profile() {
 					</>
 				)}
 			</div>
-
 			<div className="w-full border-b-2 border-b-gray-700">
-				<div className="mt-5">
+				<div className="mt-1">
 					<h3 className="font-bold ml-5 text-xl">
 						{userData?._id !== profileData?._id
 							? profileData?.fullName
@@ -207,20 +208,36 @@ export default function Profile() {
 							onClick={() => {
 								setIsLikes(false);
 								setIsPosts(true);
+								setIsMedia(false);
 							}}
 						>
 							Posts
 						</div>
+						{profileData?._id === userData?._id && (
+							<div
+								className={`flex items-center justify-center w-1/2 p-2 hover:cursor-pointer hover:bg-gray-900 ${
+									isLikes && "border-b-2 border-sky-500"
+								}`}
+								onClick={() => {
+									setIsLikes(true);
+									setIsPosts(false);
+									setIsMedia(false);
+								}}
+							>
+								Likes
+							</div>
+						)}
 						<div
 							className={`flex items-center justify-center w-1/2 p-2 hover:cursor-pointer hover:bg-gray-900 ${
 								isLikes && "border-b-2 border-sky-500"
 							}`}
 							onClick={() => {
-								setIsLikes(true);
+								setIsLikes(false);
 								setIsPosts(false);
+								setIsMedia(true);
 							}}
 						>
-							{profileData?._id === userData?._id ? "Likes" : "Media"}
+							Media
 						</div>
 					</div>
 				</div>
@@ -236,11 +253,9 @@ export default function Profile() {
 							);
 						})
 					) : (
-						<p className="text-slate-400 text-center mt-10 font-semibold">
-							Make your first post!
-						</p>
+						<FollowersSuggestions />
 					)
-				) : profileData?._id === userData?._id ? (
+				) : profileData?._id === userData?._id && isLikes ? (
 					<div className="bg-blue-950 rounded-md m-2 text-white p-2 text-sm">
 						<p>
 							<span className="mx-3">
@@ -249,13 +264,18 @@ export default function Profile() {
 							Your likes are private. Only you can see them.
 						</p>
 					</div>
-				) : (
-					<div className="bg-blue-950 rounded-md m-2 text-white p-2 text-sm">
-						<p>Media Content</p>
+				) : !attachments ? (
+					<div className="text-white p-2 flex flex-col justify-center items-center">
+						<div>
+							<h3 className="font-bold mt-10 text-5xl">
+								Lights, camera ... <br /> attachments!
+							</h3>
+							<p className="text-zinc-500 mt-3">
+								When you post photos or videos, they will show up here.
+							</p>
+						</div>
 					</div>
-				)}
-				{/* Show the follower suggestions IF the user hasn't posted anything */}
-				{/* <FollowersSuggestions /> */}
+				) : undefined}
 			</div>
 		</div>
 	);
