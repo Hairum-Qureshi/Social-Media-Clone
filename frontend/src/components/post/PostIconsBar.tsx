d402a43df9bf00010744e2c6f9d0c5c9d1623756
Hairum Qureshi <hqureshi@udel.edu>
2025-05-19 00:25:07 -0400

    added null guard for retrieving the numComments and likes

--- frontend/src/components/post/PostIconsBar.tsx
+++ frontend/src/components/post/PostIconsBar.tsx
@@ -8,7 +8,7 @@ import {
 import { faRetweet } from "@fortawesome/free-solid-svg-icons/faRetweet";
 import { IconsProps } from "../../interfaces";
 
-export default function PostIconsBar({postData}:IconsProps) {
+export default function PostIconsBar({ postData }: IconsProps) {
 	return (
 		<div
 			className="grid grid-cols-5 gap-1 text-center text-gray-600 mt-3"
@@ -16,7 +16,7 @@ export default function PostIconsBar({postData}:IconsProps) {
 		>
 			<div className="hover:text-sky-400">
 				<FontAwesomeIcon icon={faComment} />
-				<span className="ml-1">{postData.numComments}</span>
+				<span className="ml-1">{postData?.numComments}</span>
 			</div>
 			<div className="hover:text-green-400">
 				<FontAwesomeIcon icon={faRetweet} />
@@ -24,7 +24,7 @@ export default function PostIconsBar({postData}:IconsProps) {
 			</div>
 			<div className="hover:text-red-500">
 				<FontAwesomeIcon icon={faHeart} />
-				<span className="ml-1">{postData.numLikes}</span>
+				<span className="ml-1">{postData?.numLikes}</span>
 			</div>
 			<div className="hover:text-yellow-400">
 				<FontAwesomeIcon icon={faBookmark} />
