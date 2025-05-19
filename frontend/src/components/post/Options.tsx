a24c78d8ea74e23a9152b391e0fb39ffb072e117
Hairum Qureshi <hqureshi@udel.edu>
2025-05-19 11:49:16 -0400

    added logic for setting edit mode and passing the post's content ot the parent if edit mode is enabled

--- frontend/src/components/post/Options.tsx
+++ frontend/src/components/post/Options.tsx
@@ -15,96 +15,106 @@ export default function Options({
 	close,
 	isOwner,
 	username,
-	postID
+	postID,
+	isGalleryPost = false,
+	setEditMode
 }: OptionsProps) {
-	const { deleteMutation } = usePosts();
+	const { deleteMutation, postDataByID } = usePosts(undefined, postID);
 
 	return (
-		<div className="bg-black z-10 border-2 font-bold border-white p-2 h-auto absolute right-0 w-2/5 mr-5 mt-2 rounded-md">
+		<>
 			<div
-				className="absolute top-0 right-0 m-3 font-bold text-lg hover:cursor-pointer"
-				onClick={e => {
-					e.stopPropagation();
-					e.preventDefault();
-					close();
-				}}
+				className={`bg-black z-10 border-2 font-bold border-white p-2 h-auto absolute right-0 ${
+					isGalleryPost ? "w-1/4" : "w-2/5"
+				} mr-5 mt-2 rounded-md`}
 			>
-				<FontAwesomeIcon icon={faX} />
+				<div
+					className="absolute top-0 right-0 m-3 font-bold text-lg hover:cursor-pointer"
+					onClick={e => {
+						e.stopPropagation();
+						e.preventDefault();
+						close();
+					}}
+				>
+					<FontAwesomeIcon icon={faX} />
+				</div>
+				<div>
+					{isOwner && (
+						<p
+							className="my-4"
+							onClick={e => {
+								e.stopPropagation();
+								e.preventDefault();
+								setEditMode(postDataByID?.text);
+								close();
+							}}
+						>
+							<span className="mx-3">
+								<FontAwesomeIcon icon={faPencil} />
+							</span>
+							Edit Post
+						</p>
+					)}
+					{isOwner && (
+						<p
+							className="mb-4"
+							onClick={e => {
+								e.stopPropagation();
+								e.preventDefault();
+								deleteMutation(postID);
+								close();
+							}}
+						>
+							<span className="mx-3">
+								<FontAwesomeIcon icon={faTrash} />
+							</span>
+							Delete Post
+						</p>
+					)}
+					{!isOwner && (
+						<p
+							className="mb-4"
+							onClick={e => {
+								e.stopPropagation();
+								e.preventDefault();
+							}}
+						>
+							<span className="mx-3">
+								<FontAwesomeIcon icon={faUserPlus} />
+							</span>
+							Follow @{username}
+						</p>
+					)}
+					{!isOwner && (
+						<p
+							className="mb-4"
+							onClick={e => {
+								e.stopPropagation();
+								e.preventDefault();
+							}}
+						>
+							<span className="mx-3">
+								<FontAwesomeIcon icon={faUserMinus} />
+							</span>
+							Unfollow @{username}
+						</p>
+					)}
+					{!isOwner && (
+						<p
+							className="mb-4"
+							onClick={e => {
+								e.stopPropagation();
+								e.preventDefault();
+							}}
+						>
+							<span className="mx-3">
+								<FontAwesomeIcon icon={faBan} />
+							</span>
+							Block @{username}
+						</p>
+					)}
+				</div>
 			</div>
-			<div>
-				{isOwner && (
-					<p
-						className="my-4"
-						onClick={e => {
-							e.stopPropagation();
-							e.preventDefault();
-						}}
-					>
-						<span className="mx-3">
-							<FontAwesomeIcon icon={faPencil} />
-						</span>
-						Edit Post
-					</p>
-				)}
-				{isOwner && (
-					<p
-						className="mb-4"
-						onClick={e => {
-							e.stopPropagation();
-							e.preventDefault();
-							deleteMutation(postID);
-							close();
-						}}
-					>
-						<span className="mx-3">
-							<FontAwesomeIcon icon={faTrash} />
-						</span>
-						Delete Post
-					</p>
-				)}
-				{!isOwner && (
-					<p
-						className="mb-4"
-						onClick={e => {
-							e.stopPropagation();
-							e.preventDefault();
-						}}
-					>
-						<span className="mx-3">
-							<FontAwesomeIcon icon={faUserPlus} />
-						</span>
-						Follow @{username}
-					</p>
-				)}
-				{!isOwner && (
-					<p
-						className="mb-4"
-						onClick={e => {
-							e.stopPropagation();
-							e.preventDefault();
-						}}
-					>
-						<span className="mx-3">
-							<FontAwesomeIcon icon={faUserMinus} />
-						</span>
-						Unfollow @{username}
-					</p>
-				)}
-				{!isOwner && (
-					<p
-						className="mb-4"
-						onClick={e => {
-							e.stopPropagation();
-							e.preventDefault();
-						}}
-					>
-						<span className="mx-3">
-							<FontAwesomeIcon icon={faBan} />
-						</span>
-						Block @{username}
-					</p>
-				)}
-			</div>
-		</div>
+		</>
 	);
 }
