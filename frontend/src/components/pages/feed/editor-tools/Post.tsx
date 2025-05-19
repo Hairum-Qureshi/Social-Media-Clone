b7621e79a8806c4d6eddd0a8944ff2962f68b538
Hairum Qureshi <hqureshi@udel.edu>
2025-05-19 11:50:04 -0400

    added logic for rendering the PostModal component if edit mode is enabled

--- frontend/src/components/pages/feed/editor-tools/Post.tsx
+++ frontend/src/components/pages/feed/editor-tools/Post.tsx
@@ -7,86 +7,108 @@ import { PostProps } from "../../../../interfaces";
 import moment from "moment";
 import { Link, useNavigate } from "react-router-dom";
 import PostIconsBar from "../../../post/PostIconsBar";
+import PostModal from "../../../post/PostModal";
+import Editor from "./Editor";
 
 export default function Post({ isOwner, postData }: PostProps) {
 	const [showOptions, setShowOptions] = useState(false);
 	const navigate = useNavigate();
+	const [isEditMode, setIsEditMode] = useState(false);
+	const [postContent, setPostContent] = useState("");
 
 	function close() {
 		setShowOptions(false);
 	}
 
+	function setEditMode(postContent: string) {
+		setIsEditMode(!isEditMode);
+		setPostContent(postContent);
+	}
+
 	return (
-		<div
-			className="w-full p-2 border-t-2 border-b-2 border-t-gray-700 border-b-gray-700 relative hover:cursor-pointer"
-			onClick={() => navigate(`/post/${postData._id}`)}
-		>
-			{showOptions && (
-				<Options
-					close={close}
-					isOwner={isOwner}
-					username={postData.user.username}
-					postID={postData._id}
-				/>
+		<>
+			{isEditMode && (
+				<PostModal editMode={true}>
+					<Editor
+						showBorder={false}
+						placeHolder="Edit Post"
+						buttonText="CONFIRM"
+						content={postContent}
+					/>
+				</PostModal>
 			)}
-			<div className="flex items-start">
-				<img
-					src={postData.user.profilePicture}
-					alt="User pfp"
-					className="w-12 h-12 lg:w-10 lg:h-10 rounded-full object-cover"
-					onClick={e => {
-						e.stopPropagation();
-						navigate(`/${postData.user.username}`);
-					}}
-				/>
-				<div className="flex flex-col ml-3 w-full">
-					<Link
-						to={`/${postData.user.username}`}
-						onClick={e => e.stopPropagation()}
-						className="flex items-center text-base font-bold"
+			<div
+				className="w-full p-2 border-t-2 border-b-2 border-t-gray-700 border-b-gray-700 relative hover:cursor-pointer"
+				onClick={() => navigate(`/post/${postData._id}`)}
+			>
+				{showOptions && (
+					<Options
+						close={close}
+						isOwner={isOwner}
+						username={postData.user.username}
+						postID={postData._id}
+						setEditMode={setEditMode}
+					/>
+				)}
+				<div className="flex items-start">
+					<img
+						src={postData.user.profilePicture}
+						alt="User pfp"
+						className="w-12 h-12 lg:w-10 lg:h-10 rounded-full object-cover"
+						onClick={e => {
+							e.stopPropagation();
+							navigate(`/${postData.user.username}`);
+						}}
+					/>
+					<div className="flex flex-col ml-3 w-full">
+						<Link
+							to={`/${postData.user.username}`}
+							onClick={e => e.stopPropagation()}
+							className="flex items-center text-base font-bold"
+						>
+							{postData.user.fullName}&nbsp;
+							<span className="text-gray-500 font-light">
+								@{postData.user.username}
+								{postData.user.isVerified && (
+									<span
+										className="text-purple-500"
+										title="This is a verified account"
+									>
+										<FontAwesomeIcon icon={faCertificate} />
+									</span>
+								)}
+								&nbsp;·&nbsp;{moment(postData.createdAt.toString()).fromNow()}
+							</span>
+						</Link>
+						{postData.text && (
+							<div className="mt-1">
+								<span>{postData.text}</span>
+							</div>
+						)}
+						{postData.postImages?.length > 0 && (
+							<div className="w-full flex mt-2">
+								<Carousel
+									images={postData?.postImages}
+									numImages={postData?.postImages.length}
+									allowDelete={isOwner}
+									forPost={true}
+								/>
+							</div>
+						)}
+					</div>
+					<div
+						className="ml-auto mr-3 hover:cursor-pointer"
+						onClick={e => {
+							e.stopPropagation();
+							e.stopPropagation();
+							setShowOptions(!showOptions);
+						}}
 					>
-						{postData.user.fullName}&nbsp;
-						<span className="text-gray-500 font-light">
-							@{postData.user.username}
-							{postData.user.isVerified && (
-								<span
-									className="text-purple-500"
-									title="This is a verified account"
-								>
-									<FontAwesomeIcon icon={faCertificate} />
-								</span>
-							)}
-							&nbsp;·&nbsp;{moment(postData.createdAt.toString()).fromNow()}
-						</span>
-					</Link>
-					{postData.text && (
-						<div className="mt-1">
-							<span>{postData.text}</span>
-						</div>
-					)}
-					{postData.postImages?.length > 0 && (
-						<div className="w-full flex mt-2">
-							<Carousel
-								images={postData.postImages}
-								numImages={postData.postImages.length}
-								allowDelete={isOwner}
-								forPost={true}
-							/>
-						</div>
-					)}
+						<FontAwesomeIcon icon={faEllipsis} />
+					</div>
 				</div>
-				<div
-					className="ml-auto mr-3 hover:cursor-pointer"
-					onClick={e => {
-						e.stopPropagation();
-						e.stopPropagation();
-						setShowOptions(!showOptions);
-					}}
-				>
-					<FontAwesomeIcon icon={faEllipsis} />
-				</div>
+				<PostIconsBar postData={postData} />
 			</div>
-			<PostIconsBar postData={postData} />
-		</div>
+		</>
 	);
 }
