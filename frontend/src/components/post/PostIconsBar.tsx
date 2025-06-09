66b2fc8b57abb597b30262a9b471e51359f3ea00
Hairum Qureshi <hqureshi@udel.edu>
2025-06-09 09:26:47 -0400

    added 'like/unlike' functionality and styling

--- frontend/src/components/post/PostIconsBar.tsx
+++ frontend/src/components/post/PostIconsBar.tsx
@@ -10,12 +10,20 @@ import { IconsProps } from "../../interfaces";
 import usePosts from "../../hooks/usePosts";
 
 export default function PostIconsBar({ postData }: IconsProps) {
-	const { bookmarkPostMutation } = usePosts(undefined, postData?._id);
+	const { bookmarkPostMutation, likePostMutation } = usePosts(
+		undefined,
+		postData?._id
+	);
+
+	console.log(postData?.isLiked);
 
 	return (
 		<div
 			className="grid grid-cols-5 gap-1 text-center text-gray-600 mt-3"
-			onClick={e => e.stopPropagation()}
+			onClick={e => {
+				e.stopPropagation();
+				e.preventDefault();
+			}}
 		>
 			<div className="hover:text-sky-400 hover:cursor-pointer">
 				<FontAwesomeIcon icon={faComment} />
@@ -25,13 +33,26 @@ export default function PostIconsBar({ postData }: IconsProps) {
 				<FontAwesomeIcon icon={faRetweet} />
 				<span className="ml-1">{postData?.numRetweets || 0}</span>
 			</div>
-			<div className="hover:text-red-500 hover:cursor-pointer">
-				<FontAwesomeIcon icon={faHeart} />
-				<span className="ml-1">{postData?.numLikes || 0}</span>
-			</div>
+			{postData?.isLiked ? (
+				<div
+					className="hover:text-red-500 text-red-500 hover:cursor-pointer"
+					onClick={() => likePostMutation(postData?._id)}
+				>
+					<FontAwesomeIcon icon={faHeart} />
+					<span className="ml-1">{postData?.numLikes || 0}</span>
+				</div>
+			) : (
+				<div
+					className="hover:text-red-500 hover:cursor-pointer"
+					onClick={() => likePostMutation(postData?._id)}
+				>
+					<FontAwesomeIcon icon={faHeart} />
+					<span className="ml-1">{postData?.numLikes || 0}</span>
+				</div>
+			)}
 			{postData?.isBookmarked ? (
 				<div
-					className="hover:text-yellow-400 text-yellow-400"
+					className="hover:text-yellow-400 text-yellow-400 hover:cursor-pointer"
 					onClick={() => bookmarkPostMutation(postData?._id)}
 				>
 					<FontAwesomeIcon icon={faBookmark} />
@@ -39,14 +60,22 @@ export default function PostIconsBar({ postData }: IconsProps) {
 				</div>
 			) : (
 				<div
-					className="hover:text-yellow-400"
+					className="hover:text-yellow-400 hover:cursor-pointer"
 					onClick={() => bookmarkPostMutation(postData?._id)}
 				>
 					<FontAwesomeIcon icon={faBookmark} />
 					<span className="ml-1">{postData?.numBookmarks || 0}</span>
 				</div>
 			)}
-			<div className="hover:text-sky-400 hover:cursor-pointer">
+			<div
+				className="hover:text-sky-400 hover:cursor-pointer"
+				onClick={async () => {
+					alert("Copied to clipboard");
+					await navigator.clipboard.writeText(
+						`${import.meta.env.VITE_BACKEND_BASE_URL}/post/${postData?._id}`
+					);
+				}}
+			>
 				<FontAwesomeIcon icon={faShare} />
 			</div>
 		</div>
