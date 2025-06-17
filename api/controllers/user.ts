9bf8669318305d6b7d37c29dc8ecd000a14836a2
Hairum Qureshi <hqureshi@udel.edu>
2025-06-17 00:19:10 -0400

    added 'addExtendedBioWorkExperience' and 'getExtendedBioData' function controllers

--- api/controllers/user.ts
+++ api/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { IUser, PostImage, UserData } from "../interfaces";
+import { IUser, PostImage, UserData, IWorkHistory } from "../interfaces";
 import User from "../models/User";
 import Notification from "../models/Notification";
 import mongoose, { Types } from "mongoose";
@@ -10,6 +10,29 @@ import fs from "fs";
 import { handleProfileImageUploads } from "../lib/utils/handleProfileImageUploads";
 import Post from "../models/Post";
 import sanitizeHtml from "sanitize-html";
+import WorkHistory from "../models/WorkHistory";
+
+const ALLOWED_HTML_TAGS = [
+	"h1",
+	"h2",
+	"h3",
+	"h4",
+	"h5",
+	"h6",
+	"blockquote",
+	"li",
+	"ol",
+	"p",
+	"ul",
+	"a",
+	"b",
+	"br",
+	"em",
+	"i",
+	"s",
+	"strong",
+	"u"
+];
 
 const getProfile = async (req: Request, res: Response): Promise<void> => {
 	// TODO - Need to exclude 'password' from the populate methods using select
@@ -443,27 +466,7 @@ const addExtendedBio = async (req: Request, res: Response): Promise<void> => {
 		}
 
 		const cleanHTML = sanitizeHtml(extendedBioContent, {
-			allowedTags: [
-				"h1",
-				"h2",
-				"h3",
-				"h4",
-				"h5",
-				"h6",
-				"blockquote",
-				"li",
-				"ol",
-				"p",
-				"ul",
-				"a",
-				"b",
-				"br",
-				"em",
-				"i",
-				"s",
-				"strong",
-				"u"
-			],
+			allowedTags: ALLOWED_HTML_TAGS,
 			allowedAttributes: {
 				a: ["href"]
 			}
@@ -496,11 +499,14 @@ const deleteExtendedBio = async (
 	res: Response
 ): Promise<void> => {
 	try {
+		const currUID: Types.ObjectId = req.user._id;
+		const currUserData: IUser = (await User.findOne({ _id: currUID })) as IUser;
 
-		const currUID:Types.ObjectId = req.user._id;
-		const currUserData:IUser = await User.findOne({ _id: currUID }) as IUser;
-
-		if(!currUserData?.extendedBio || currUserData?.extendedBio.length === 0 || currUserData?.extendedBio.length === 7) {
+		if (
+			!currUserData?.extendedBio ||
+			currUserData?.extendedBio.length === 0 ||
+			currUserData?.extendedBio.length === 7
+		) {
 			res.status(400).json({ message: "Extended bio is already empty" });
 			return;
 		}
@@ -518,10 +524,94 @@ const deleteExtendedBio = async (
 		res.status(200).json({
 			updatedUser
 		});
-	}
-	catch (error) {
+	} catch (error) {
 		console.error(
 			"Error in user.ts file, addExtendedBio function controller".red.bold,
+			error
+		);
+		res.status(500).json({ message: (error as Error).message });
+	}
+};
+
+const addExtendedBioWorkExperience = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const {
+			isCurrentlyWorkingHere,
+			jobTitle,
+			companyName,
+			location,
+			startDate,
+			endDate,
+			experience
+		} = req.body;
+
+		const currUID: Types.ObjectId = req.user._id;
+
+		const workHistory: IWorkHistory = await WorkHistory.create({
+			user: currUID,
+			company: companyName,
+			jobTitle,
+			location,
+			currentlyWorkingThere: isCurrentlyWorkingHere,
+			startDate,
+			endDate: !isCurrentlyWorkingHere ? endDate : "",
+			description: experience
+		});
+
+		const updatedUser: IUser = (await User.findByIdAndUpdate(
+			currUID,
+			{
+				$push: { workHistory: workHistory._id }
+			},
+			{
+				new: true
+			}
+		)) as IUser;
+
+		res.status(201).json(updatedUser);
+	} catch (error) {
+		console.error(
+			"Error in user.ts file, addExtendedBioWorkExperience function controller"
+				.red.bold,
+			error
+		);
+		res.status(500).json({ message: (error as Error).message });
+	}
+};
+
+const getExtendedBioData = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const { username } = req.params;
+
+		const user: IUser | null = await User.findOne({ username });
+
+		if (!user) {
+			res.status(404).json({ message: "User not found" });
+			return;
+		}
+
+		const userExtendedBio: IUser = (await User.findOne({ username }).populate(
+			"workHistory"
+		)) as IUser;
+
+		res.status(201).json({
+			extendedBio: userExtendedBio.extendedBio || undefined,
+			userData: {
+				username: userExtendedBio.username,
+				fullName: userExtendedBio.fullName,
+				profilePicture: userExtendedBio.profilePicture
+			},
+			workExperience: userExtendedBio.workHistory || undefined
+		});
+	} catch (error) {
+		console.error(
+			"Error in user.ts file, getExtendedBioData function controller".red.bold,
 			error
 		);
 		res.status(500).json({ message: (error as Error).message });
@@ -537,5 +627,7 @@ export {
 	updateProfileBackdrop,
 	getPostsImages,
 	addExtendedBio,
-	deleteExtendedBio
+	deleteExtendedBio,
+	addExtendedBioWorkExperience,
+	getExtendedBioData
 };
