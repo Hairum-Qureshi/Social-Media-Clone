123adcda845419621f9e32786be6abe0c1e83a64
Hairum Qureshi <hqureshi@udel.edu>
2025-06-17 00:31:44 -0400

    updated path to user route file

--- api/index.ts
+++ api/index.ts
@@ -21,8 +21,6 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// const app = express();
-
 const corsOptions = {
 	origin: ["http://localhost:5174", "http://localhost:5173"],
 	credentials: true,
