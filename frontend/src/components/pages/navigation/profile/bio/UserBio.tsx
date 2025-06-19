d757b164f5e55c92a1b1faa4b4c8644c79ac5548
Hairum Qureshi <hqureshi@udel.edu>
2025-06-19 12:12:11 -0400

    added delete work history button functionality and fixed bugs involving current user not being able to edit their extended bio

--- frontend/src/components/pages/navigation/profile/bio/UserBio.tsx
+++ frontend/src/components/pages/navigation/profile/bio/UserBio.tsx
@@ -10,17 +10,33 @@ import { useEffect, useState } from "react";
 import TipTapEditor from "./editor/TipTapEditor";
 import useProfile from "../../../../../hooks/useProfile";
 import { useLocation, useNavigate } from "react-router-dom";
-import { EditorTypes } from "../../../../../interfaces";
+import { EditorTypes, WorkHistory } from "../../../../../interfaces";
+import WorkHistoryForm from "./form/WorkHistoryForm";
+import { formatDateRange } from "../../../../../utils/formatDateRange";
 
 export default function UserBio() {
 	const { userData } = useAuthContext()!;
 	const [showEditor, setEditorVisibility] = useState(false);
 	const [showWorkHistory, setShowWorkHistory] = useState(false);
 	const [editorContent, setEditorContent] = useState("");
-	const { addExtendedBio, deleteExtendedBio } = useProfile();
+	const {
+		addExtendedBio,
+		deleteExtendedBio,
+		extendedBio,
+		deleteWorkExperienceByID
+	} = useProfile();
+
+	// TODO - add a loading animation when the save button is pressed
+	// TODO - make it so that you can click anywhere in the editor and be able to type instead of just the beginning
+	// TODO - have it so that if the URL contains another user's username, display their bio; it currently just shows the current user's
+	// TODO - need to add elapsed time from start date to end date
+	// TODO - need to prevent empty inputs
+	// TODO - need to add 'read more' to work history divs
+	// ! formatDateRange() function does not work
 
 	const location = useLocation();
 	const [pathname, setPathname] = useState(location.pathname.split("/"));
+	const pathnameLength = location.pathname.split("/").length;
 
 	useEffect(() => {
 		setPathname(location.pathname.split("/"));
@@ -31,11 +47,21 @@ export default function UserBio() {
 	}
 
 	const navigate = useNavigate();
+	const isAnotherUserProfile: boolean = location.pathname.includes(
+		"/settings/bio"
+	)
+		? false
+		: pathnameLength === 3 && pathname[1] !== userData?.username;
+
+	function hideWorkHistoryForm() {
+		setShowWorkHistory(false);
+	}
 
 	// TODO - need to add a character limit for the extended bio
+	// ! Fix issue where the current user is unable to update their extended bio (clicking on the div won't work)
 
 	return (
-		<div className="bg-black text-white min-h-screen overflow-auto relative mx-auto w-full">
+		<div className="bg-black text-white min-h-screen overflow-auto mx-auto w-full max-w-screen-xl">
 			<div className="w-full p-2 flex items-center">
 				<div
 					className="text-xl ml-5 hover:cursor-pointer"
@@ -45,18 +71,35 @@ export default function UserBio() {
 				</div>
 				<div className="ml-5 flex items-center">
 					<img
-						src={userData?.profilePicture}
+						src={
+							isAnotherUserProfile
+								? extendedBio?.userData?.profilePicture
+								: userData?.profilePicture
+						}
 						alt="User profile picture"
 						className="w-10 h-10 rounded-full object-cover"
 					/>
 					<div className="flex flex-col ml-3 text-base">
-						<span className="ml-2 font-semibold">{userData?.fullName}</span>
-						<span className="ml-2 text-slate-500">@{userData?.username}</span>
+						<span className="ml-2 font-semibold">
+							{isAnotherUserProfile && extendedBio
+								? extendedBio?.userData?.fullName
+								: userData?.fullName}
+						</span>
+						<span className="ml-2 text-slate-500">
+							@
+							{isAnotherUserProfile && extendedBio
+								? extendedBio?.userData?.username
+								: userData?.username}
+						</span>
 					</div>
 				</div>
 				{pathname[1] === userData?.username && pathname[1] !== "settings" && (
 					<div className="ml-auto border border-white rounded-lg p-0.5 w-16 text-center">
-						<button onClick={() => setEditorVisibility(true)}>Edit</button>
+						<button
+							onClick={() => !isAnotherUserProfile && setEditorVisibility(true)}
+						>
+							Edit
+						</button>
 					</div>
 				)}
 			</div>
@@ -78,24 +121,29 @@ export default function UserBio() {
 				</div>
 				{!showEditor ? (
 					<div
-						className={`bg-gray-800 rounded-md p-3 mt-3 flex  ${
+						className={`bg-neutral-900 border border-slate-700 rounded-md p-3 mt-3 flex ${
 							pathname[1] !== userData?.username &&
 							pathname[1] === "settings" &&
 							"hover:cursor-pointer"
 						}`}
 						onClick={() => {
-							if (
-								pathname[1] === userData?.username &&
-								pathname[1] !== "settings"
-							) {
+							if (isAnotherUserProfile) {
 								return;
 							}
 
 							setEditorVisibility(true);
 						}}
 					>
-						{!userData?.extendedBio && (
-							<p className="text-base">Tell us about yourself</p>
+						{pathnameLength === 3 &&
+						pathname[1] !== userData?.username &&
+						!extendedBio?.extendedBio ? (
+							<p className="text-base">
+								{pathname[1]} currently hasn't written a bio
+							</p>
+						) : (
+							!userData?.extendedBio && (
+								<p className="text-base">Tell us about yourself</p>
+							)
 						)}
 						<div
 							className="leading-5
@@ -108,7 +156,9 @@ export default function UserBio() {
                 [&_li]:mb-1
                 [&_a]:text-sky-400 [&_a]:underline [&_a]:hover:text-sky-300 prose prose-invert prose-headings:font-artegra"
 							dangerouslySetInnerHTML={{
-								__html: userData?.extendedBio || ""
+								__html: isAnotherUserProfile
+									? extendedBio?.extendedBio || ""
+									: userData?.extendedBio || ""
 							}}
 						/>
 						{pathname[1] !== userData?.username &&
@@ -172,38 +222,89 @@ export default function UserBio() {
 					)}
 				</div>
 				{!showWorkHistory ? (
-					<div
-						className={`bg-gray-800 rounded-md p-3 mt-3 flex ${
-							pathname[1] !== userData?.username &&
-							pathname[1] === "settings" &&
-							"hover:cursor-pointer"
-						}`}
-						onClick={() =>
-							pathname[1] !== userData?.username &&
-							pathname[1] === "settings" &&
-							setShowWorkHistory(true)
-						}
-					>
-						<p className="text-base">Add experience</p>
+					<>
+						<div>
+							{extendedBio?.workExperience &&
+								extendedBio?.workExperience.length > 0 &&
+								extendedBio.workExperience.map(
+									(workExperience: WorkHistory) => {
+										return (
+											<div className="bg-neutral-900 border border-slate-700 rounded-md p-3 mt-3 flex">
+												<div className="w-full">
+													<p className="font-semibold">
+														{workExperience.jobTitle}
+													</p>
+													<p className="text-sm text-zinc-400">
+														{workExperience.company}&nbsp;·&nbsp;
+														{workExperience.location}
+													</p>
+													<p className="text-sm text-zinc-400">
+														{workExperience.startDate}&nbsp;-&nbsp;
+														{!workExperience.endDate
+															? "Present"
+															: workExperience.endDate}
+														{workExperience.endDate && (
+															<p>
+																{formatDateRange(
+																	workExperience.startDate.toString(),
+																	workExperience.endDate.toString()
+																)}
+															</p>
+														)}
+													</p>
+													<div
+														className="text-sm mt-1"
+														dangerouslySetInnerHTML={{
+															__html: workExperience.description
+														}}
+													></div>
+												</div>
+												{location.pathname.includes("settings") &&
+													extendedBio?.userData.username ===
+														userData?.username && (
+														<div className="ml-auto flex">
+															<FontAwesomeIcon
+																icon={faTrashCan}
+																className="mx-3 text-red-600 hover:cursor-pointer"
+																onClick={() =>
+																	deleteWorkExperienceByID(workExperience._id)
+																}
+															/>
+															<FontAwesomeIcon
+																icon={faPencil}
+																className="hover:cursor-pointer"
+															/>
+														</div>
+													)}
+											</div>
+										);
+									}
+								)}
+						</div>
+						<div
+							className={`bg-neutral-900 border border-slate-700 rounded-md p-3 mt-3 flex ${
+								pathname[1] !== userData?.username &&
+								pathname[1] === "settings" &&
+								"hover:cursor-pointer"
+							}`}
+							onClick={() =>
+								pathname[1] !== userData?.username &&
+								pathname[1] === "settings" &&
+								setShowWorkHistory(true)
+							}
+						>
+							<p className="text-base">Add experience</p>
 
-						{pathname[1] !== userData?.username &&
-							pathname[1] === "settings" && (
-								<span className="ml-auto">
-									<FontAwesomeIcon icon={faCirclePlus} />
-								</span>
-							)}
-					</div>
+							{pathname[1] !== userData?.username &&
+								pathname[1] === "settings" && (
+									<span className="ml-auto">
+										<FontAwesomeIcon icon={faCirclePlus} />
+									</span>
+								)}
+						</div>
+					</>
 				) : (
-					<div className="border border-slate-500 mt-2 rounded-md max-w-full">
-						<TipTapEditor
-							getEditorContent={getEditorContent}
-							editorFor={EditorTypes.WORK_HISTORY}
-							showBlockQuoteButton={false}
-							showEmojiButton={false}
-							showLinkButton={false}
-							showTextSizesSelector={false}
-						/>
-					</div>
+					<WorkHistoryForm hideWorkHistoryForm = {hideWorkHistoryForm} />
 				)}
 			</div>
 		</div>
