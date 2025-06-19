2a23ad9cb46105b2280c63b94c7dad4094c535d6
Hairum Qureshi <hqureshi@udel.edu>
2025-06-18 22:45:00 -0400

    added new middleware that checks for ownership for routes handling modifying or deleting resources

--- api/routes/post.ts
+++ api/routes/post.ts
@@ -5,7 +5,8 @@ import {
 	getAllLikedPosts,
 	getFollowingUsersPosts,
 	pinPost,
-	getSearchedPhrase
+	getSearchedPhrase,
+	retweetPost
 } from "../controllers/post-related/post";
 import multer from "multer";
 import { storage } from "../config/multer-config";
@@ -22,13 +23,17 @@ import {
 	getAllBookmarkedPosts,
 	handleBookmarking
 } from "../controllers/post-related/bookmarking-logic";
-import { deleteComment, postComment } from "../controllers/post-related/crud-operations/comment-crud-ops";
+import {
+	deleteComment,
+	postComment
+} from "../controllers/post-related/crud-operations/comment-crud-ops";
+import checkOwner from "../middleware/checkOwner";
 
 export const upload = multer({ storage });
 const router = express.Router();
 
 router.post("/create", checkAuthStatus, upload.array("images", 4), createPost);
-router.delete("/:postID", checkAuthStatus, deletePost);
+router.delete("/:postID", checkAuthStatus, checkOwner("post"), deletePost);
 router.post("/comment/:postID", checkAuthStatus, postComment);
 router.get("/all-liked", checkAuthStatus, getAllLikedPosts);
 router.get("/all", checkAuthStatus, getAllPosts);
@@ -38,10 +43,11 @@ router.get("/current-user/all", checkAuthStatus, getAllCurrUserPosts);
 router.delete("/comment/:commentID/:postID", checkAuthStatus, deleteComment);
 router.get("/bookmarked", checkAuthStatus, getSearchedPhrase);
 router.get("/:postID", checkAuthStatus, getPostData);
-router.patch("/:postID/edit", checkAuthStatus, editPost);
-router.patch("/:postID/pin", checkAuthStatus, pinPost);
+router.patch("/:postID/edit", checkAuthStatus, checkOwner("post"), editPost);
+router.patch("/:postID/pin", checkAuthStatus, checkOwner("post"), pinPost);
 router.patch("/:postID/bookmark", checkAuthStatus, handleBookmarking);
 router.get("/bookmarked/all", checkAuthStatus, getAllBookmarkedPosts);
 router.patch("/:postID/like", checkAuthStatus, handleLikes);
+router.patch("/:postID/re-tweet", checkAuthStatus, retweetPost);
 
 export default router;
