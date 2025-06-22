3fb570295fc263062401542465e25484405bc799
Hairum Qureshi <hqureshi@udel.edu>
2025-06-22 09:08:00 -0400

    added logic to display the company logos alongside the work experience info

--- frontend/src/components/pages/navigation/profile/bio/WorkHistory.tsx
+++ frontend/src/components/pages/navigation/profile/bio/WorkHistory.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useProfile from "../../../../../hooks/useProfile";
 import { WorkHistory as IWorkHistory } from "../../../../../interfaces";
 import { formatDateRange } from "../../../../../utils/formatDateRange";
@@ -16,6 +16,10 @@ export default function WorkHistory() {
 	const [showWorkHistory, setShowWorkHistory] = useState(false);
 	const location = useLocation();
 	const [pathname] = useState(location.pathname.split("/"));
+	const [workHistoryIndex, setWorkHistoryIndex] = useState(-1);
+	const [workHistoryToEdit, setWorkHistoryToEdit] = useState<
+		IWorkHistory | undefined
+	>();
 
 	function hideWorkHistoryForm() {
 		setShowWorkHistory(false);
@@ -24,6 +28,17 @@ export default function WorkHistory() {
 	const { extendedBio, deleteWorkExperienceByID } = useProfile();
 	const { userData } = useAuthContext()!;
 
+	useEffect(() => {
+		const workHistory: IWorkHistory[] | undefined = extendedBio?.workExperience;
+
+		if (workHistory && workHistory.length > 0 && workHistoryIndex !== -1) {
+			const toEdit: IWorkHistory = workHistory[workHistoryIndex];
+			setWorkHistoryToEdit(toEdit);
+		}
+	}, [workHistoryIndex]);
+
+	// TODO - make edit work history feature work
+
 	return (
 		<div className="w-full p-5">
 			<div className="flex items-center">
@@ -31,7 +46,11 @@ export default function WorkHistory() {
 				{showWorkHistory && (
 					<h1
 						className="ml-auto text-red-500 hover:cursor-pointer"
-						onClick={() => setShowWorkHistory(false)}
+						onClick={() => {
+							setShowWorkHistory(false);
+							setWorkHistoryIndex(-1);
+							setWorkHistoryToEdit(undefined);
+						}}
 					>
 						Cancel
 					</h1>
@@ -42,55 +61,73 @@ export default function WorkHistory() {
 					<div>
 						{extendedBio?.workExperience &&
 							extendedBio?.workExperience.length > 0 &&
-							extendedBio.workExperience.map((workExperience: IWorkHistory) => {
-								return (
-									<div className="bg-neutral-900 border border-slate-700 rounded-md p-3 mt-3 flex">
-										<div className="w-full">
-											<p className="font-semibold">{workExperience.jobTitle}</p>
-											<p className="text-sm text-zinc-400">
-												{workExperience.company}&nbsp;·&nbsp;
-												{workExperience.location}
-											</p>
-											<p className="text-sm text-zinc-400">
-												{workExperience.startDate}&nbsp;-&nbsp;
-												{!workExperience.endDate
-													? "Present"
-													: workExperience.endDate}
-												{workExperience.endDate && (
-													<p>
-														{formatDateRange(
-															workExperience.startDate.toString(),
-															workExperience.endDate.toString()
-														)}
-													</p>
-												)}
-											</p>
-											<div
-												className="text-sm mt-1"
-												dangerouslySetInnerHTML={{
-													__html: workExperience.description
-												}}
-											></div>
-										</div>
-										{location.pathname.includes("settings") &&
-											extendedBio?.userData.username === userData?.username && (
-												<div className="ml-auto flex">
-													<FontAwesomeIcon
-														icon={faTrashCan}
-														className="mx-3 text-red-600 hover:cursor-pointer"
-														onClick={() =>
-															deleteWorkExperienceByID(workExperience._id)
-														}
-													/>
-													<FontAwesomeIcon
-														icon={faPencil}
-														className="hover:cursor-pointer"
+							extendedBio.workExperience.map(
+								(workExperience: IWorkHistory, index: number) => {
+									return (
+										<div className="bg-neutral-900 border border-slate-700 rounded-md p-3 mt-3 flex">
+											<div className="w-full flex">
+												<div className="w-10 h-10 mr-3">
+													<img
+														src={workExperience.companyLogo}
+														alt="Company Logo"
+														className="w-10 h-10 object-cover rounded-sm"
 													/>
 												</div>
-											)}
-									</div>
-								);
-							})}
+												<div>
+													<p className="font-semibold">
+														{workExperience.jobTitle}
+													</p>
+													<p className="text-sm text-zinc-400">
+														{workExperience.company}&nbsp;·&nbsp;
+														{workExperience.location}
+													</p>
+													<p className="text-sm text-zinc-400">
+														{workExperience.startDate}&nbsp;-&nbsp;
+														{!workExperience.endDate
+															? "Present"
+															: workExperience.endDate}
+														{workExperience.endDate && (
+															<p>
+																{formatDateRange(
+																	workExperience.startDate.toString(),
+																	workExperience.endDate.toString()
+																)}
+															</p>
+														)}
+													</p>
+													<div
+														className="text-sm mt-1"
+														dangerouslySetInnerHTML={{
+															__html: workExperience.description
+														}}
+													></div>
+												</div>
+											</div>
+											{location.pathname.includes("settings") &&
+												extendedBio?.userData.username ===
+													userData?.username && (
+													<div className="ml-auto flex">
+														<FontAwesomeIcon
+															icon={faTrashCan}
+															className="mx-3 text-red-600 hover:cursor-pointer"
+															onClick={() =>
+																deleteWorkExperienceByID(workExperience._id)
+															}
+														/>
+														<FontAwesomeIcon
+															icon={faPencil}
+															className="hover:cursor-pointer"
+															onClick={() => {
+																setShowWorkHistory(true);
+																setWorkHistoryIndex(index);
+															}}
+														/>
+													</div>
+												)}
+										</div>
+									);
+								}
+							)}
 					</div>
 					<div
 						className={`bg-neutral-900 border border-slate-700 rounded-md p-3 mt-3 flex ${
@@ -115,7 +152,10 @@ export default function WorkHistory() {
 					</div>
 				</>
 			) : (
-				<WorkHistoryForm hideWorkHistoryForm={hideWorkHistoryForm} />
+				<WorkHistoryForm
+					hideWorkHistoryForm={hideWorkHistoryForm}
+					workHistoryToEdit={workHistoryToEdit}
+				/>
 			)}
 		</div>
 	);
