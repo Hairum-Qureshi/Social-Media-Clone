7b8c179cb8f69078860c171d902aa11df2cbd2f3
Hairum Qureshi <hqureshi@udel.edu>
2025-06-22 07:58:07 -0400

    added logic to convert a notification into a link if the notification object contains a link, otherwise it's unclickable

--- frontend/src/components/pages/navigation/notification/Notifications.tsx
+++ frontend/src/components/pages/navigation/notification/Notifications.tsx
@@ -4,33 +4,53 @@ import useNotifications from "../../../../hooks/useNotifications";
 import { Notification } from "../../../../interfaces";
 import UserNotificationCard from "./UserNotificationCard";
 import { getDescription } from "../../../../utils/getDescription";
+import { Link } from "react-router-dom";
 
 export default function Notifications() {
-	const { notificationData } = useNotifications();
+	const { notificationData, isLoading, deleteAllNotifications } =
+		useNotifications();
 
 	return (
 		<div className="bg-black w-full text-white min-h-screen overflow-auto">
 			<div className="flex items-center">
 				<h2 className="text-2xl font-bold m-5">Notifications</h2>
-				<span className="text-2xl ml-auto m-5 hover:cursor-pointer">
+				<span
+					className="text-2xl ml-auto m-5 hover:cursor-pointer"
+					onClick={deleteAllNotifications}
+				>
 					<FontAwesomeIcon icon={faGear} />
 				</span>
 			</div>
-			{!notificationData || notificationData.length === 0 ? (
+			{isLoading ? (
+				<h3 className="text-xl font-semibold text-center mt-10">Loading...</h3>
+			) : !notificationData || notificationData.length === 0 ? (
 				<h3 className="text-xl font-semibold text-center mt-10">
 					You currently don't have any notifications
 				</h3>
 			) : (
-				notificationData.map((notification: Notification) => (
-					<UserNotificationCard
-						username={notification.from.username}
-						userPfp={notification.from.profilePicture}
-						notifType={notification.notifType}
-						notifDescription={getDescription(notification.notifType)}
-						notifDate={notification.createdAt.toString()}
-						notifID={notification._id}
-					/>
-				))
+				notificationData.map((notification: Notification) =>
+					notification.link ? (
+						<Link to={notification.link}>
+							<UserNotificationCard
+								username={notification.from.username}
+								userPfp={notification.from.profilePicture}
+								notifType={notification.notifType}
+								notifDescription={getDescription(notification.notifType)}
+								notifDate={notification.createdAt.toString()}
+								notifID={notification._id}
+							/>
+						</Link>
+					) : (
+						<UserNotificationCard
+							username={notification.from.username}
+							userPfp={notification.from.profilePicture}
+							notifType={notification.notifType}
+							notifDescription={getDescription(notification.notifType)}
+							notifDate={notification.createdAt.toString()}
+							notifID={notification._id}
+						/>
+					)
+				)
 			)}
 		</div>
 	);
