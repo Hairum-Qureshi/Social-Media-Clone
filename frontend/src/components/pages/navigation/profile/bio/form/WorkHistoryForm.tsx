ab1e82b792e3c3ede338c2ffddc2faac89fe8885
Hairum Qureshi <hqureshi@udel.edu>
2025-06-23 09:37:20 -0400

    added conditional check to remove the chosen end month if the user checks the 'is currently working here' checkbox

--- frontend/src/components/pages/navigation/profile/bio/form/WorkHistoryForm.tsx
+++ frontend/src/components/pages/navigation/profile/bio/form/WorkHistoryForm.tsx
@@ -5,50 +5,33 @@ import {
 } from "../../../../../../interfaces";
 import TipTapEditor from "../editor/TipTapEditor";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MonthsDropDown from "./dropdowns/MonthsDropDown";
 import YearsDropDown from "./dropdowns/YearsDropDown";
 import useProfile from "../../../../../../hooks/useProfile";
 import CompaniesDropDown from "./dropdowns/CompaniesDropDown";
 
-// ! issue - animated placeholder hides text in input
+// TODO - make the drop down for the company close if it can't the logo for the company
+// ! BUG: for some reason there's an issue typing in the editor when trying to edit work history
 
 export default function WorkHistoryForm({
 	hideWorkHistoryForm,
 	workHistoryToEdit
 }: WorkHistoryFormProps) {
-	const [isCurrentlyWorkingHere, setIsCurrentlyWorkingHere] = useState(
-		workHistoryToEdit ? workHistoryToEdit.currentlyWorkThere : false
-	);
-	const [jobTitle, setJobTitle] = useState(
-		workHistoryToEdit ? workHistoryToEdit.jobTitle : ""
-	);
-	const [companyName, setCompanyName] = useState(
-		workHistoryToEdit ? workHistoryToEdit.company : ""
-	);
-	const [location, setLocation] = useState(
-		workHistoryToEdit ? workHistoryToEdit.location : ""
-	);
-	const [experience, setExperience] = useState(
-		workHistoryToEdit ? workHistoryToEdit.description : ""
-	);
+	const [isCurrentlyWorkingHere, setIsCurrentlyWorkingHere] = useState(false);
+	const [jobTitle, setJobTitle] = useState("");
+	const [companyName, setCompanyName] = useState("");
+	const [location, setLocation] = useState("");
+	const [experience, setExperience] = useState("");
 	const [showStartMonths, setShowStartMonths] = useState(false);
 	const [showEndMonths, setShowEndMonths] = useState(false);
-	const [chosenStartMonth, setChosenStartMonth] = useState(
-		workHistoryToEdit ? workHistoryToEdit.startDate.split(" ")[0] : ""
-	);
-	const [chosenEndMonth, setChosenEndMonth] = useState(
-		workHistoryToEdit ? workHistoryToEdit.endDate.split(" ")[0] : ""
-	);
+	const [chosenStartMonth, setChosenStartMonth] = useState("");
+	const [chosenEndMonth, setChosenEndMonth] = useState("");
 	const [selectedStartYear, setSelectedStartYear] = useState<string>(
-		workHistoryToEdit
-			? workHistoryToEdit.startDate.split(" ").pop()!
-			: new Date().getFullYear().toString()
+		new Date().getFullYear().toString()
 	);
 	const [selectedEndYear, setSelectedEndYear] = useState<string>(
-		workHistoryToEdit
-			? workHistoryToEdit.endDate.split(" ").pop()!
-			: new Date().getFullYear().toString()
+		new Date().getFullYear().toString()
 	);
 	const [showSelectedStartYear, setShowSelectedStartYear] = useState(false);
 	const [showSelectedEndYear, setShowSelectedEndYear] = useState(false);
@@ -57,30 +40,22 @@ export default function WorkHistoryForm({
 
 	function handleMonthSelection(month: string) {
 		if (showStartMonths) {
-			setChosenStartMonth(
-				workHistoryToEdit ? workHistoryToEdit.startDate.split(" ")[0] : month
-			);
+			setChosenStartMonth(month);
 			setShowStartMonths(false);
 		}
 		if (showEndMonths) {
-			setChosenEndMonth(
-				workHistoryToEdit ? workHistoryToEdit.endDate.split(" ")[0] : month
-			);
+			setChosenEndMonth(month);
 			setShowEndMonths(false);
 		}
 	}
 
 	function handleYearSelection(year: string) {
 		if (showSelectedStartYear) {
-			setSelectedStartYear(
-				workHistoryToEdit ? workHistoryToEdit.startDate.split(" ").pop()! : year
-			);
+			setSelectedStartYear(year);
 			setShowSelectedStartYear(false);
 		}
 		if (showSelectedEndYear) {
-			setSelectedEndYear(
-				workHistoryToEdit ? workHistoryToEdit.endDate.split(" ").pop()! : year
-			);
+			setSelectedEndYear(year);
 			setShowSelectedEndYear(false);
 		}
 	}
@@ -95,49 +70,52 @@ export default function WorkHistoryForm({
 		setShowCompaniesDropDown(false);
 	}
 
-	const { addExtendedBioWorkExperience } = useProfile();
+	useEffect(() => {
+		if (workHistoryToEdit) {
+			setJobTitle(workHistoryToEdit.jobTitle);
+			setCompanyName(workHistoryToEdit.company);
+			setLocation(workHistoryToEdit.location);
+			setExperience(workHistoryToEdit.description);
+			setChosenStartMonth(workHistoryToEdit.startDate.split(" ")[0]);
+			setSelectedStartYear(workHistoryToEdit.startDate.split(" ").pop()!);
+
+			if (workHistoryToEdit.currentlyWorkingThere) {
+				setIsCurrentlyWorkingHere(true);
+			} else {
+				setChosenEndMonth(workHistoryToEdit.endDate.split(" ")[0]);
+				setSelectedEndYear(workHistoryToEdit.endDate.split(" ").pop()!);
+			}
+		}
+	}, [workHistoryToEdit]);
+
+	const { addExtendedBioWorkExperience, updateExtendedBioWorkExperience } =
+		useProfile();
 
 	return (
 		<div className="mt-3 border border-slate-700 p-2 rounded-md bg-zinc-900">
-			<h3 className="text-white text-lg font-semibold">Add Experience</h3>
+			<h3 className="text-white text-lg font-semibold">
+				{workHistoryToEdit ? "Edit Experience" : "Add Experience"}
+			</h3>
 			<div className="relative mt-6 w-full">
 				<input
 					type="text"
 					id="jobTitle"
 					placeholder="Job Title"
-					className="peer p-2 w-full rounded-md outline-none border border-slate-500 bg-transparent text-white placeholder-transparent"
-					value={workHistoryToEdit ? workHistoryToEdit.jobTitle : jobTitle}
+					className="peer p-2 w-full rounded-md outline-none border border-slate-500 bg-transparent text-white"
+					value={jobTitle}
 					onChange={e => setJobTitle(e.target.value)}
 				/>
-				<label
-					htmlFor="jobTitle"
-					className="absolute left-2 top-2 text-slate-400 text-sm bg-zinc-900 px-1 transition-all duration-200 
-               peer-placeholder-shown:top-2 peer-placeholder-shown:text-base 
-               peer-placeholder-shown:text-slate-500 
-               peer-focus:top-[-0.6rem] peer-focus:text-sm peer-focus:text-slate-500"
-				>
-					Job Title
-				</label>
 			</div>
 			<div className="relative mt-6 w-full">
 				<input
 					type="text"
 					id="company"
 					placeholder="Company"
-					className="peer p-2 w-full rounded-md outline-none border border-slate-500 bg-transparent text-white placeholder-transparent"
-					value={workHistoryToEdit ? workHistoryToEdit.company : companyName}
+					className="peer p-2 w-full rounded-md outline-none border border-slate-500 bg-transparent text-white"
+					value={companyName}
 					onChange={e => setCompanyName(e.target.value)}
 					onKeyDown={() => setShowCompaniesDropDown(true)}
 				/>
-				<label
-					htmlFor="company"
-					className="absolute left-2 top-2 text-slate-400 text-sm bg-zinc-900 px-1 transition-all duration-200 
-               peer-placeholder-shown:top-2 peer-placeholder-shown:text-base 
-               peer-placeholder-shown:text-slate-500 
-               peer-focus:top-[-0.6rem] peer-focus:text-sm peer-focus:text-slate-500"
-				>
-					Company
-				</label>
 				{companyName && showCompaniesDropDown && (
 					<CompaniesDropDown company={companyName} setCompany={setCompany} />
 				)}
@@ -147,26 +125,23 @@ export default function WorkHistoryForm({
 					type="text"
 					id="location"
 					placeholder="Location"
-					className="peer p-2 w-full rounded-md outline-none border border-slate-500 bg-transparent text-white placeholder-transparent"
-					value={workHistoryToEdit ? workHistoryToEdit.location : location}
+					className="peer p-2 w-full rounded-md outline-none border border-slate-500 bg-transparent text-white"
+					value={location}
 					onChange={e => setLocation(e.target.value)}
 				/>
-				<label
-					htmlFor="location"
-					className="absolute left-2 top-2 text-slate-400 text-sm bg-zinc-900 px-1 transition-all duration-200 
-               peer-placeholder-shown:top-2 peer-placeholder-shown:text-base 
-               peer-placeholder-shown:text-slate-500 
-               peer-focus:top-[-0.6rem] peer-focus:text-sm peer-focus:text-slate-500"
-				>
-					Location
-				</label>
 			</div>
 			<div className="flex items-center">
 				<input
 					type="checkbox"
 					placeholder="Job Title"
 					className="border border-slate-500 w-3.5 h-3.5"
-					onChange={() => setIsCurrentlyWorkingHere(!isCurrentlyWorkingHere)}
+					onChange={() => {
+						setIsCurrentlyWorkingHere(!isCurrentlyWorkingHere);
+						if (!isCurrentlyWorkingHere && chosenEndMonth) {
+							setChosenEndMonth("");
+						}
+					}}
+					checked={isCurrentlyWorkingHere}
 				/>
 				<label htmlFor="" className="ml-2">
 					Currently work here
@@ -290,7 +265,7 @@ export default function WorkHistoryForm({
 						showEmojiButton={false}
 						showLinkButton={false}
 						showTextSizesSelector={false}
-						workExperienceDescription={workHistoryToEdit?.description}
+						workExperienceDescription={experience}
 					/>
 				</div>
 			</div>
@@ -299,15 +274,17 @@ export default function WorkHistoryForm({
 					<button
 						className="p-1 rounded-md bg-sky-500 w-28 ml-auto"
 						onClick={() => {
-							// addExtendedBioWorkExperience(
-							// 	isCurrentlyWorkingHere,
-							// 	jobTitle,
-							// 	companyName,
-							// 	location,
-							// 	`${chosenStartMonth} ${selectedStartYear}`,
-							// 	`${chosenEndMonth} ${selectedEndYear}`,
-							// 	experience
-							// );
+							updateExtendedBioWorkExperience(
+								workHistoryToEdit?._id,
+								isCurrentlyWorkingHere,
+								jobTitle,
+								companyName,
+								companyLogo,
+								location,
+								`${chosenStartMonth} ${selectedStartYear}`,
+								`${chosenEndMonth} ${selectedEndYear}`,
+								experience
+							);
 							hideWorkHistoryForm();
 						}}
 					>
