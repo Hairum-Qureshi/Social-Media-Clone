9563c950ef0a6dfcad47086b8fb8b20de145d48c
Hairum Qureshi <hqureshi@udel.edu>
2025-06-23 21:14:26 -0400

    removed util function for generating AES key pair

--- api/lib/utils/checkIfBookmarked.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from "mongoose";
-import { IPost } from "../../interfaces";
-
-export function checkIfBookmarked(post: IPost, currUID: Types.ObjectId): boolean {
-	return post.bookmarkedBy.some((uid: Types.ObjectId) => uid.equals(currUID));
-}
--- api/lib/utils/generateKeyPair.ts
+++
@@ -1,9 +0,0 @@
-import forge from "node-forge";
-import { KeyPair } from "../../interfaces";
-
-export function generateKeyPair(): KeyPair {
-	const keypair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
-	const publicKeyPem = forge.pki.publicKeyToPem(keypair.publicKey);
-	const privateKeyPem = forge.pki.privateKeyToPem(keypair.privateKey);
-	return { publicKeyPem, privateKeyPem };
-};
