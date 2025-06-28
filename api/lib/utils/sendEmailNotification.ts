c6d319e4b56e2689fdbb20c5aa4b37ddce44818e
Hairum Qureshi <hqureshi@udel.edu>
2025-06-28 18:48:18 -0400

    added Nodemailer email sending logic to send DM request email notifications

---
+++ api/lib/utils/sendEmailNotification.ts
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+function callEmailAuth(): nodemailer.Transporter {
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		port: 587,
+		secure: false,
+		auth: {
+			user: process.env.EMAIL,
+			pass: process.env.APP_PASS
+		}
+	});
+
+	return transporter;
+}
+
+export async function sendEmailNotification(fromEmail:string, toEmail:string, toName:string, message:string, fromUsername:string) {
+	try {
+		const transporter = callEmailAuth();
+		await transporter.sendMail({
+			from: fromEmail,
+			to: `${toName} <${toEmail}>`,
+			subject: `@${fromUsername} sent you a DM request on X-Clone!`,
+			text: message 
+		});
+	} catch (error) {
+		"<nodemailer.ts> sendEmail function error".yellow,
+			(error as Error).toString().red.bold;
+	}
+}
