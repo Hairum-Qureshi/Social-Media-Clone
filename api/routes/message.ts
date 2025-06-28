d8eec851d7327a950a77a45644bf7c20c2b63efc
Hairum Qureshi <hqureshi@udel.edu>
2025-06-28 18:52:45 -0400

    added 'dm requests' GET route

--- api/routes/message.ts
+++ api/routes/message.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { getAllConversations, getSearchedUsers } from "../controllers/message";
+import {
+	getAllConversations,
+	getSearchedUsers,
+	createDM,
+	getConversationChat,
+	postMessage,
+	getDMRequests
+} from "../controllers/message";
 
 const router = express.Router();
 
 router.post("/searched-users", checkAuthStatus, getSearchedUsers);
 router.get("/conversations", checkAuthStatus, getAllConversations);
+router.get("/conversation/:conversationID", checkAuthStatus, getConversationChat);
+router.post("/create", checkAuthStatus, createDM);
+router.post("/new-message/:conversationID", checkAuthStatus, postMessage);
+router.get("/dm-requests", checkAuthStatus, getDMRequests);
 
 export default router;
