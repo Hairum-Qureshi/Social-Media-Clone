e673866c2bbdbf29a0d97274b8da33d8b3b92b7d
Hairum Qureshi <hqureshi@udel.edu>
2025-07-02 07:01:49 -0400

    added new PATCH route for accepting DM requests and updated import path for 'getDMRequests' function controller

--- api/routes/message.ts
+++ api/routes/message.ts
@@ -5,17 +5,31 @@ import {
 	getSearchedUsers,
 	createDM,
 	getConversationChat,
-	postMessage,
+	postMessage
+} from "../controllers/message-related/message";
+import {
+	acceptDMRequest,
 	getDMRequests
-} from "../controllers/message";
+} from "../controllers/message-related/dm-request-related/handle-dm-requests";
 
 const router = express.Router();
 
 router.post("/searched-users", checkAuthStatus, getSearchedUsers);
 router.get("/conversations", checkAuthStatus, getAllConversations);
-router.get("/conversation/:conversationID", checkAuthStatus, getConversationChat);
+router.get(
+	"/conversation/:conversationID",
+	checkAuthStatus,
+	getConversationChat
+);
 router.post("/create", checkAuthStatus, createDM);
 router.post("/new-message/:conversationID", checkAuthStatus, postMessage);
 router.get("/dm-requests", checkAuthStatus, getDMRequests);
+
+// TODO - add middleware to prevent another user from accepting/declining another user's DM request
+router.patch(
+	"/dm-requests/:requestID/accept",
+	checkAuthStatus,
+	acceptDMRequest
+);
 
 export default router;
