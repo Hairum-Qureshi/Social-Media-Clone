c67813a6ec20ced65cfeb589ff8e147488f50c78
Hairum Qureshi <hqureshi@udel.edu>
2025-07-02 06:09:50 -0400

    made tweaks to the logic of checking whether the user follows another user

--- frontend/src/utils/checkFollowingStatus.ts
+++ frontend/src/utils/checkFollowingStatus.ts
@@ -3,13 +3,21 @@ import { UserData, UserData_Conversation } from "../interfaces";
 export default function isFollowing(
 	profile: UserData | UserData_Conversation | undefined,
 	currUID: string | undefined
+	// isProfilePage: boolean = true
 ): boolean {
 	if (profile && profile._id !== currUID) {
-		const profileFollowers = profile.followers;
-		return profileFollowers.some(
-			(follower: UserData) => follower._id === currUID
+		const profileFollowing: string[] = profile.following as unknown as string[];
+		return profileFollowing?.some(
+			(following: string) => String(following) === String(currUID)
 		);
 	}
+
+	// if (profile && profile._id !== currUID && isProfilePage) {
+	// 	const profileFollowers: string[] = profile.followers as unknown as string[];
+	// 	return profileFollowers.some(
+	// 		(follower: string) => String(follower) === String(currUID)
+	// 	);
+	// }
 
 	return false;
 }
