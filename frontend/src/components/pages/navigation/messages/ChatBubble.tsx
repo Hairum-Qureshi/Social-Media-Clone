b07ac5778a461883927b3c6128db0b49a5a63d63
Hairum Qureshi <hqureshi@udel.edu>
2025-07-09 19:52:29 -0400

    added new default optional prop, 'isSystem' and added logic checking if it's a system message and if it is, style it appropriately

--- frontend/src/components/pages/navigation/messages/ChatBubble.tsx
+++ frontend/src/components/pages/navigation/messages/ChatBubble.tsx
@@ -1,14 +1,36 @@
 import moment from "moment";
 import { ChatBubbleProps } from "../../../../interfaces";
+import useAuthContext from "../../../../contexts/AuthContext";
 
 // TODO - design the chat bubble so it looks like a bubble
 
 export default function ChatBubble({
 	you,
 	message,
-	timestamp
+	timestamp,
+	isSystem = false
 }: ChatBubbleProps) {
-	return (
+	const { userData } = useAuthContext()!;
+
+	return isSystem ? (
+		<div>
+			<div className="w-full flex justify-center">
+				<div
+					className="bg-sky-950 w-11/12 text-sky-300 text-sm italic px-3 py-1.5 rounded-sm border border-sky-500 shadow-sm"
+					title={moment(timestamp).fromNow()}
+				>
+					<p>
+						{userData && message.includes(userData.username)
+							? message.replace(`@${userData.username}`, "you")
+							: message}
+					</p>
+				</div>
+			</div>
+			<p className="text-right text-xs text-gray-300 mt-1 mr-2">
+				{moment(timestamp).format("ddd h:mm A")}
+			</p>
+		</div>
+	) : (
 		<div
 			className={`flex flex-col m-2 ${
 				you ? "items-end" : "items-start"
