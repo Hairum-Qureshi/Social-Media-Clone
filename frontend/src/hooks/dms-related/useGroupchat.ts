672bff58550d63d1a68f7af30d0825751cd617ff
Hairum Qureshi <hqureshi@udel.edu>
2025-07-09 20:05:31 -0400

    created 'useGroupchat' hook

--- frontend/src/hooks/dms-related/useGroupchat.ts
+++ frontend/src/hooks/dms-related/useGroupchat.ts
@@ -1,12 +1,35 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { GroupChatTools } from "../../interfaces";
 
-interface GroupChatTools {
+export default function useGroupChat(): GroupChatTools {
+	const queryClient = useQueryClient();
 
-}
+	const { mutate: makeAdminMutation } = useMutation({
+		mutationFn: async ({
+			conversationID,
+			uid
+		}: {
+			conversationID: string;
+			uid: string;
+		}) => {
+			const response = await axios.patch(
+				`${
+					import.meta.env.VITE_BACKEND_BASE_URL
+				}/api/messages/conversations/${conversationID}/make-admin`,
+				{ uid },
+				{ withCredentials: true }
+			);
+			return response.data;
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["conversations"] });
+		}
+	});
 
-export default function useGroupChat():GroupChatTools {
-    const queryClient = useQueryClient();
+	const makeAdmin = (conversationID: string, uid: string) => {
+		makeAdminMutation({ conversationID, uid });
+	};
 
-
-    return {  }
+	return { makeAdmin };
 }
