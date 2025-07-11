2d5b7dd32e47d894b3819416735117514c5b1f9b
Hairum Qureshi <hqureshi@udel.edu>
2025-07-11 06:02:46 -0400

    created middleware checking if the current user is an admin for the groupchat

---
+++ api/middleware/checkAdminStatus.ts
@@ -0,0 +1,50 @@
+import { Request, Response, NextFunction } from "express";
+import { IConversation, IUser } from "../interfaces";
+import Conversation from "../models/inbox/Conversation";
+import User from "../models/User";
+import { Types } from "mongoose";
+
+export default async function checkAuthStatus(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const { conversationID } = req.params;
+		const { uid } = req.body;
+		const currUID: Types.ObjectId = req.user._id;
+
+		const conversation: IConversation | undefined =
+			(await Conversation.findById(conversationID)) as
+				| IConversation
+				| undefined;
+
+		if (!conversation) {
+			res.status(404).json({ message: "Conversation not found" });
+			return;
+		}
+
+		const existingUser: IUser | undefined = (await User.findById(uid)) as
+			| IUser
+			| undefined;
+
+		if (!existingUser) {
+			res.status(404).json({ message: "User not found" });
+			return;
+		}
+
+		if (!conversation?.admins.includes(currUID)) {
+			res
+				.status(403)
+				.json({ message: "You are not an admin of this conversation" });
+			return;
+		}
+
+		next();
+	} catch (error) {
+		console.error(
+			"Error in checkAdminStatus.ts middleware file".red.bold,
+			error
+		);
+	}
+}
