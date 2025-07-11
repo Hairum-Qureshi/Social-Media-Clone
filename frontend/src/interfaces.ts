5404868c68c6c2266dd7dc399558c18af7b5a354
Hairum Qureshi <hqureshi@udel.edu>
2025-07-11 17:03:29 -0400

    renamed 'showSearchModal' to 'showUserSearchModal' in InboxINfoPanelProps interface

--- frontend/src/interfaces.ts
+++ frontend/src/interfaces.ts
@@ -503,7 +503,7 @@ export interface ContactsProps {
 export interface InboxInfoPanelProps {
 	conversationData: Conversation | DMRequest;
 	showGCRenameModal: (show: boolean) => void;
-	showSearchModal: (show:boolean) => void;
+	showUserSearchModal: (show:boolean) => void;
 }
 
 export interface GroupChatTools {
