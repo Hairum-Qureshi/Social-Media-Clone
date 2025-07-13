287b6936a0fcbe88cdc66a5ea1b6af7fcd65e5ab
Hairum Qureshi <hqureshi@udel.edu>
2025-07-13 09:09:20 -0400

    added route to update the group chat photo

--- api/routes/message-related/groupchat-related.ts
+++ api/routes/message-related/groupchat-related.ts
@@ -1,31 +1,54 @@
 import express from "express";
 import checkAuthStatus from "../../middleware/checkAuthStatus";
 import checkAdminStatus from "../../middleware/checkAdminStatus";
-import { leaveGroupChat, makeAdmin, removeUserFromGroupChat, renameGroupChat } from "../../controllers/message-related/groupchat-related";
+import {
+	addUsersToGroupChat,
+	leaveGroupChat,
+	makeAdmin,
+	removeUserFromGroupChat,
+	renameGroupChat,
+	uploadGroupPhoto
+} from "../../controllers/message-related/groupchat-related";
+import { upload } from "../post";
 
 const router = express.Router();
 
 router.patch(
-    "/conversations/:conversationID/make-admin",
-    checkAuthStatus,
-    checkAdminStatus,
-    makeAdmin
+	"/conversations/:conversationID/make-admin",
+	checkAuthStatus,
+	checkAdminStatus,
+	makeAdmin
 );
 router.patch(
-    "/conversations/:conversationID/remove-user",
-    checkAuthStatus,
-    checkAdminStatus,
-    removeUserFromGroupChat
+	"/conversations/:conversationID/remove-user",
+	checkAuthStatus,
+	checkAdminStatus,
+	removeUserFromGroupChat
 );
 router.patch(
-    "/conversations/:conversationID/leave",
-    checkAuthStatus,
-    leaveGroupChat
+	"/conversations/:conversationID/leave",
+	checkAuthStatus,
+	leaveGroupChat
 );
 router.patch(
-    "/conversations/:conversationID/rename",
-    checkAuthStatus,
-    renameGroupChat
+	"/conversations/:conversationID/rename",
+	checkAuthStatus,
+	renameGroupChat
+);
+
+// ! NOTE: the checkAdminStatus middleware won't work for this because this will take in an array of uids which is why it's omitted for the time being
+router.patch(
+	"/conversations/:conversationID/add-users",
+	checkAuthStatus,
+	addUsersToGroupChat
+);
+
+router.patch(
+	"/conversations/:conversationID/group-photo",
+	checkAuthStatus,
+	checkAdminStatus,
+	upload.single("group-photo"),
+	uploadGroupPhoto
 );
 
 export default router;
