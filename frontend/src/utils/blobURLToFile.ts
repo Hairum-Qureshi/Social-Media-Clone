74850bab181ca5ea06627fa5467f8ad8eab671a2
Hairum Qureshi <hqureshi@udel.edu>
2025-07-13 09:40:40 -0400

    switched 'uploadType' and 'userID' params, made 'userID' optional, and added conditional to check if 'uploadType' is a groupPhoto

--- frontend/src/utils/blobURLToFile.ts
+++ frontend/src/utils/blobURLToFile.ts
@@ -2,24 +2,29 @@ import axios from "axios";
 
 export async function blobURLToFile(
 	blobURL: string,
-	userID: string,
 	uploadType: string,
-	postID?: string
+	userID?: string,
+	postID?: string,
+	conversationID?: string
 ): Promise<File> {
 	const response = await axios.get(blobURL, { responseType: "blob" });
 	const blob = response.data;
 	const originalFile = await fetch(blobURL).then(res => res.blob());
 
-	if (postID && uploadType === "post") {
+	if (postID && userID && uploadType === "post") {
 		return new File(
 			[blob],
-			`${postID}-${userID}-${Math.round(
-				Date.now() * Math.random() * 1e9
-			)}`,
+			`${postID}-${userID}-${Math.round(Date.now() * Math.random() * 1e9)}`,
 			{
 				type: originalFile.type
 			}
 		);
+	}
+
+	if (uploadType === "groupPhoto") {
+		return new File([blob], `${conversationID}-groupPhoto`, {
+			type: originalFile.type
+		});
 	}
 
 	if (uploadType === "profile-picture") {
