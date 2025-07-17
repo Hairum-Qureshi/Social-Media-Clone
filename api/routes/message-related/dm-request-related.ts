d8678b95593594d256492bcf2268d01d398d9af4
Hairum Qureshi <hqureshi@udel.edu>
2025-07-17 07:47:22 -0400

    added route for users to delete DM requests

--- api/routes/message-related/dm-request-related.ts
+++ api/routes/message-related/dm-request-related.ts
@@ -2,23 +2,25 @@ import express from "express";
 import checkAuthStatus from "../../middleware/checkAuthStatus";
 import {
 	acceptDMRequest,
+	deleteDMRequest,
 	getDMRequests
 } from "../../controllers/message-related/handle-dm-requests";
-import checkOwner from "../../middleware/checkOwner";
 
 const router = express.Router();
 
 router.get("/dm-requests", checkAuthStatus, getDMRequests);
+
+// TODO - maybe add middleware (checkOwner?) to prevent another user from accepting/deleting requests 
 router.patch(
 	"/dm-requests/:requestID/accept",
 	checkAuthStatus,
 	acceptDMRequest
 );
-// router.delete(
-// 	"/dm-requests/:requestID",
-// 	checkAuthStatus,
-// 	checkOwner("DM Request"),
-// 	acceptDMRequest
-// );
+
+router.delete(
+	"/dm-requests/:requestID/delete",
+	checkAuthStatus, 
+	deleteDMRequest
+);
 
 export default router;
