46d030509511c909d0a90d21877b7e7db57d388b
Hairum Qureshi <hqureshi@udel.edu>
2025-07-18 18:27:52 -0400

    created helper function for incrementing the notification count

---
+++ api/utils/IncrementNotificationCount.ts
@@ -0,0 +1,11 @@
+import { Types } from "mongoose";
+import User from "../models/User";
+import { IUser } from "../interfaces";
+
+export async function incrementNotificationCount(currUID: Types.ObjectId) {
+	const numNotifications = ((await User.findById(currUID)) as IUser)
+		.numNotifications;
+	await User.findByIdAndUpdate(currUID, {
+		numNotifications: numNotifications + 1
+	});
+}
