5254ecb62dbd5d7f886f332a989e5ba8246b9298
Hairum Qureshi <hqureshi@udel.edu>
2025-07-18 18:34:38 -0400

    added missing 'hasReadNotifications' property to also update when incrementing the notification count

--- api/utils/IncrementNotificationCount.ts
+++ api/utils/IncrementNotificationCount.ts
@@ -6,6 +6,7 @@ export async function incrementNotificationCount(currUID: Types.ObjectId) {
 	const numNotifications = ((await User.findById(currUID)) as IUser)
 		.numNotifications;
 	await User.findByIdAndUpdate(currUID, {
-		numNotifications: numNotifications + 1
+		numNotifications: numNotifications + 1,
+		hasReadNotifications: false
 	});
 }
