d452afa4f88c400ea0427963af5299a47d9f4aab
Hairum Qureshi <hqureshi@udel.edu>
2025-07-20 14:22:19 -0400

    added logic to map over the 'getFollowReccs' array and if there's no users to follow, it would just let the user know

--- frontend/src/components/pages/feed/suggestions/FollowersSuggestions.tsx
+++ frontend/src/components/pages/feed/suggestions/FollowersSuggestions.tsx
@@ -1,14 +1,29 @@
+import useFeed from "../../../../hooks/useFeed";
+import { UserData } from "../../../../interfaces";
 import UserCard from "../UserCard";
 
 export default function FollowersSuggestions() {
+	const { getFollowReccs } = useFeed();
+
 	return (
 		<div className="rounded-md bg-zinc-900 my-5 mx-3 p-2 hidden lg:block">
 			<h1 className="font-bold text-xl m-3">Who to follow</h1>
-			<UserCard showFollowStatus={false} />
-			<UserCard showFollowStatus={false} />
-			<UserCard showFollowStatus={false} />
-			<UserCard showFollowStatus={false} />
-			<UserCard showFollowStatus={false} />
+			{getFollowReccs?.length > 0 ? (
+				getFollowReccs?.map((user: UserData) => {
+					return (
+						<UserCard
+							showFollowButton={true}
+							isFollowing={false}
+							showFollowStatus={false}
+							userData={user}
+						/>
+					);
+				})
+			) : (
+				<h3 className="text-center font-semibold">
+					There are currently no users to follow
+				</h3>
+			)}
 		</div>
 	);
 }
