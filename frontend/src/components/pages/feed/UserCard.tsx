e238885c5a21d13a411c0304faf6c58cad4939ce
Hairum Qureshi <hqureshi@udel.edu>
2025-07-20 14:19:57 -0400

    fixed logic for rendering the 'follow/unfollow' text

--- frontend/src/components/pages/feed/UserCard.tsx
+++ frontend/src/components/pages/feed/UserCard.tsx
@@ -1,6 +1,7 @@
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { UserCardProps } from "../../../interfaces";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useProfile from "../../../hooks/useProfile";
 
 export default function UserCard({
 	showFollowButton = true,
@@ -9,20 +10,25 @@ export default function UserCard({
 	userData
 }: UserCardProps) {
 	// TODO - make sure to render out the verified badges too if the user is verified
+	// TODO - make the follow/unfollow buttons work
+	// TODO - add text if the user has no followers/isn't following anyone
+
+	const { handleFollowing } = useProfile();
+
 	return (
 		<div className="p-2">
 			<div className="flex items-center justify-center">
 				<img
-					src={userData.profilePicture}
+					src={userData?.profilePicture}
 					alt="User pfp"
 					className="lg:w-10 lg:h-10 w-12 h-12 rounded-full"
 				/>
 				<div className="hidden lg:flex lg:flex-col lg:w-full lg:text-left lg:ml-3">
 					<span className="text-base font-bold flex justify-between items-center">
-						{userData.fullName}
+						{userData?.fullName}
 					</span>
 					<span className="text-sm text-gray-500 font-light">
-						@{userData.username}
+						@{userData?.username}
 					</span>
 					{showFollowStatus
 						? isFollowing && (
@@ -33,13 +39,22 @@ export default function UserCard({
 						  )
 						: undefined}
 				</div>
-				{showFollowButton && (
+				{showFollowButton && !isFollowing ? (
 					<div>
-						<button className="p-2 w-20 bg-white text-black rounded-full text-sm font-bold">
+						<button
+							className="p-2 w-20 bg-white text-black rounded-full text-sm font-bold"
+							onClick={() => handleFollowing(userData?._id)}
+						>
 							Follow
 						</button>
 					</div>
-				)}
+				) : showFollowButton && isFollowing ? (
+					<div>
+						<button className="p-2 w-20 bg-white text-black rounded-full text-sm font-bold">
+							Unfollow
+						</button>
+					</div>
+				) : undefined}
 			</div>
 		</div>
 	);
