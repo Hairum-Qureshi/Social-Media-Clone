38b08723ccd614d948d413878895733454ce7f5b
Hairum Qureshi <hqureshi@udel.edu>
2025-07-20 04:24:57 -0400

    added route to mark all notifs as read and clear the count back to 0

--- api/routes/notification.ts
+++ api/routes/notification.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import checkAuthStatus from "../middleware/checkAuthStatus";
-import { getAllNotifications, deleteNotification, deleteNotifications } from "../controllers/notification";
+import { getAllNotifications, deleteNotification, deleteNotifications, markAllNotifsAsRead } from "../controllers/notification";
 
 const router = express.Router();
 
 router.get('/all', checkAuthStatus, getAllNotifications);
 router.delete('/all', checkAuthStatus, deleteNotifications);
 router.delete("/:notificationID", checkAuthStatus, deleteNotification);
+router.put("/mark-all-read", checkAuthStatus, markAllNotifsAsRead);
 
 export default router;
