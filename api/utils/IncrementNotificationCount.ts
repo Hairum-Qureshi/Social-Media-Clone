a76f416f1b7d657ad3027c2ac8739d7282c2d29b
Hairum Qureshi <hqureshi@udel.edu>
2025-07-20 04:30:21 -0400

    replaced 'currUID' with 'userID'

--- api/utils/IncrementNotificationCount.ts
+++ api/utils/IncrementNotificationCount.ts
@@ -2,11 +2,17 @@ import { Types } from "mongoose";
 import User from "../models/User";
 import { IUser } from "../interfaces";
 
-export async function incrementNotificationCount(currUID: Types.ObjectId) {
-	const numNotifications = ((await User.findById(currUID)) as IUser)
-		.numNotifications;
-	await User.findByIdAndUpdate(currUID, {
-		numNotifications: numNotifications + 1,
-		hasReadNotifications: false
-	});
+export async function incrementNotificationCount(
+	userID: Types.ObjectId
+): Promise<number> {
+	const updatedUser: IUser = (await User.findByIdAndUpdate(
+		userID,
+		{
+			$inc: { numNotifications: 1 },
+			hasReadNotifications: false
+		},
+		{ new: true }
+	)) as IUser;
+
+	return updatedUser.numNotifications;
 }
